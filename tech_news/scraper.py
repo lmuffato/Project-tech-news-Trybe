57580d047d104fc0974d8200808e7acad3f73c1b
mallmann02 <leomallmann020302@gmail.com>
2022-01-11 18:11:14 -0300

    feat: req3

--- tech_news/scraper.py
+++ tech_news/scraper.py
@@ -13,25 +13,28 @@ def fetch(url):
         response = None
     finally:
         if response.status_code == 200:
-            return response
+            return response.text
         else:
             return None
 
 
 # Requisito 2
 def scrape_novidades(html_content):
-    selector = Selector(html_content.text)
+    selector = Selector(html_content)
     news_item = selector.css('.tec--list__item').getall()
     news_url = []
     for item in news_item:
-        url = item.css('article figure a::href')
+        url = item.css('article figure a::attr(href)')
         news_url.append(url)
     return news_url
 
 
 # Requisito 3
 def scrape_next_page_link(html_content):
-    """Seu código deve vir aqui"""
+    selector = Selector(html_content)
+    next_page_link_css_selector = '.tec--list.tec--list--lg > a::attr(href)'
+    next_page_link = selector.css(next_page_link_css_selector).get()
+    return next_page_link
 
 
 # Requisito 4
