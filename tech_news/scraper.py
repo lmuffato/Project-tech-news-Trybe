23f31b56e9e5e7d8c7653388075600215e751965
Rafael Medeiros Gomes <rafafamg@gmail.com>
2022-01-11 15:32:09 -0300

    feat: :sparkles: added fetch function

--- tech_news/scraper.py
+++ tech_news/scraper.py
@@ -1,6 +1,20 @@
+from typing import Union
+import requests
+import time
+
+from requests.models import Response
+
+
 # Requisito 1
-def fetch(url):
-    """Seu código deve vir aqui"""
+def fetch(url: str) -> Union[Response, None]:
+    time.sleep(1)
+    try:
+        response = requests.get(url, timeout=3)
+        if(response.status_code != 200):
+            return None
+        return response.text
+    except requests.exceptions.Timeout:
+        return None
 
 
 # Requisito 2
