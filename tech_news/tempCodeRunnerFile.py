4542fc4361e580910a9fbb3eb0ef47ff68f731d4
Flavio Franco <flavio.win@gmail.com>
2022-01-11 09:02:32 -0300

    requisito 1

--- tech_news/tempCodeRunnerFile.py
+++ tech_news/tempCodeRunnerFile.py
@@ -16,21 +16,5 @@ def fetch(url):
         return None
 
 
-# Requisito 2
-def scrape_novidades(html_content):
-    """Seu código deve vir aqui"""
-
-
-# Requisito 3
-def scrape_next_page_link(html_content):
-    """Seu código deve vir aqui"""
-
-
-# Requisito 4
-def scrape_noticia(html_content):
-    """Seu código deve vir aqui"""
-
-
-# Requisito 5
-def get_tech_news(amount):
-    """Seu código deve vir aqui"""
+x = fetch(URL_BASE)
+print(x)
