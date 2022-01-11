59529d729e6cecf9d644d5166e0378fda7cee382
Ana Ventura <anaventura1811@gmail.com>
2022-01-11 14:45:16 -0300

    feat: req. 1 do projeto

--- tech_news/scraper.py
+++ tech_news/scraper.py
@@ -1,6 +1,20 @@
+import requests
+import time
+
+
 # Requisito 1
 def fetch(url):
-    """Seu código deve vir aqui"""
+    try:
+        response = requests.get(url, timeout=3)
+        time.sleep(1)
+        if response.status_code == 200:
+            return response.text
+        else:
+            return None
+    except requests.Timeout:
+        return None
+
+# print(fetch("http://quotes.toscrape.com/page/1"))
 
 
 # Requisito 2
