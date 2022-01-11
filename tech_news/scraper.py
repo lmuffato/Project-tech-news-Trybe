e475381069d06d29a6a26f74c242563f4479fe26
Eduardo Costa Maciel <costaeduardomaciel@gmail.com>
2022-01-11 16:00:04 -0300

    Feat: Implementa funçao 'fetch'

--- tech_news/scraper.py
+++ tech_news/scraper.py
@@ -1,23 +1,44 @@
+import requests
+import time
+
+# path = "https://www.tecmundo.com.br/novidades"
+
+
 # Requisito 1
 def fetch(url):
-    """Seu código deve vir aqui"""
+    time.sleep(1)
+    try:
+        res = requests.get(url, timeout=3)
+        if res.status_code == 200:
+            return res.text
+
+        return None
+    except requests.Timeout:
+        return None
+
+
+# print(fetch(path))
 
 
 # Requisito 2
 def scrape_novidades(html_content):
     """Seu código deve vir aqui"""
+    pass
 
 
 # Requisito 3
 def scrape_next_page_link(html_content):
     """Seu código deve vir aqui"""
+    pass
 
 
 # Requisito 4
 def scrape_noticia(html_content):
     """Seu código deve vir aqui"""
+    pass
 
 
 # Requisito 5
 def get_tech_news(amount):
     """Seu código deve vir aqui"""
+    pass
