941bf97ffa51e9ed40233f22044a3bafb0da66d1
iago <iago.pferreiravr@gmail.com>
2022-01-11 14:48:32 -0300

    feat(Ratings): Req10
    Implementa a função top_5_news no arquivo rating.py
    
    E também cria a função auxiliar calculate_popularity

--- tech_news/analyzer/ratings.py
+++ tech_news/analyzer/ratings.py
@@ -1,6 +1,21 @@
+from tech_news.database import find_news
+
+# import operator
+
+
+def calculate_popularity(new):
+    return new["shares_count"] + new["comments_count"]
+
+
 # Requisito 10
 def top_5_news():
-    """Seu código deve vir aqui"""
+    news = find_news()
+    if not news:
+        return []
+
+    news.sort(reverse=True, key=calculate_popularity)
+
+    return [(new["title"], new["url"]) for new in news[:5]]
 
 
 # Requisito 11
