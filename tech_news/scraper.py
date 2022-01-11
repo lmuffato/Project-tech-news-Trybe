fe3b058e27374a17b85ebb51706588e75f3c4a31
jonathan <jonathanfernandesjf98@gmail.com>
2022-01-11 15:52:44 -0300

    iniciando projeto com requisito 01

--- tech_news/scraper.py
+++ tech_news/scraper.py
@@ -1,7 +1,17 @@
+import requests
+import time
+
 # Requisito 1
 def fetch(url):
     """Seu código deve vir aqui"""
-
+    time.sleep(1)
+    try:
+        response = requests.get(url, timeout=3)  
+    except requests.ReadTimeout:
+        return None
+    if response.status_code and response.status_code == 200:
+        return response.text 
+    return None
 
 # Requisito 2
 def scrape_novidades(html_content):
