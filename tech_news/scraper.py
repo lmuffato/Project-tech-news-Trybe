bfe447d8ec09217a529e0fb11ec836c57a44f99d
Bruno Mendes <mendessbruno@gmail.com>
2022-01-11 16:13:50 -0300

    resolvido desafio 3

--- tech_news/scraper.py
+++ tech_news/scraper.py
@@ -29,7 +29,12 @@ def scrape_novidades(html_content):
 
 # Requisito 3
 def scrape_next_page_link(html_content):
-    """Seu código deve vir aqui"""
+    selector = parsel.Selector(html_content)
+    next_page_link = selector.css("a.tec--btn::attr(href)").get()
+    if next_page_link:
+        return next_page_link
+    else:
+        return None
 
 
 # Requisito 4
