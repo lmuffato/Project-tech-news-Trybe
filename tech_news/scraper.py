4c8539040b190191c9c088ee46c785e4eab54c1d
Marcelo Maurício <mmauricio.design@gmail.com>
2022-01-11 15:16:07 -0300

    Feat 1: created a function to make a fetch with a url, and return data with the conditions

--- tech_news/scraper.py
+++ tech_news/scraper.py
@@ -1,23 +1,45 @@
+import requests
+import time
+
 # Requisito 1
+
+
 def fetch(url):
-    """Iniciando o projeto"""
+    try:
+        time.sleep(1)
+        response = requests.get(url, timeout=3)
+    except requests.ReadTimeout:
+        return None
+
+    if response.status_code != 200:
+        return None
+    else:
+        return response.text
 
 
 # Requisito 2
+
+
 def scrape_novidades(html_content):
     """Seu código deve vir aqui"""
 
 
 # Requisito 3
+
+
 def scrape_next_page_link(html_content):
     """Seu código deve vir aqui"""
 
 
 # Requisito 4
+
+
 def scrape_noticia(html_content):
     """Seu código deve vir aqui"""
 
 
 # Requisito 5
+
+
 def get_tech_news(amount):
     """Seu código deve vir aqui"""
