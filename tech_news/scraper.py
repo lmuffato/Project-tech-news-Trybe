31dc80a9e068988f8f46b0db68c11f2d3d936961
MarcioFujita <hanker78@gmail.com>
2022-01-11 14:49:42 -0300

    feat: Requisito 1

--- tech_news/scraper.py
+++ tech_news/scraper.py
@@ -1,6 +1,20 @@
+import requests
+import time
+
+
 # Requisito 1
 def fetch(url):
     """Seu código deve vir aqui"""
+    try:
+        time.sleep(1)
+        response = requests.get(url, timeout=3)
+    except requests.ReadTimeout:
+        return None
+
+    if (response.status_code != 200):
+        return None
+
+    return response.text
 
 
 # Requisito 2
