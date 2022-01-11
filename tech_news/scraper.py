d398a7e3c8ffe10d51bada39445937b02695bf58
Jodiel Briesemeister <jodielll29@gmail.com>
2022-01-11 18:49:08 -0300

    feat(req3): add scrape_next_page_link

--- tech_news/scraper.py
+++ tech_news/scraper.py
@@ -25,7 +25,9 @@ def scrape_novidades(html_content):
 
 # Requisito 3
 def scrape_next_page_link(html_content):
-    """Seu código deve vir aqui"""
+    selector = Selector(text=html_content)
+    next_page_link = selector.css(".tec--list--lg > a::attr(href)").get()
+    return next_page_link
 
 
 # Requisito 4
