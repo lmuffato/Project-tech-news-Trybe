4ea437f1a76aafed54a3ab64d98807bb5dfc910f
Viegas <mvrdgs@gmail.com>
2022-01-12 11:09:13 -0300

    requisito 4

--- tech_news/scraper.py
+++ tech_news/scraper.py
@@ -36,7 +36,56 @@ def scrape_next_page_link(html_content):
 
 # Requisito 4
 def scrape_noticia(html_content):
-    """Seu código deve vir aqui"""
+    selector = Selector(html_content)
+    url = selector.css("head link[rel=canonical]::attr(href)").get()
+
+    title = selector.css("#js-article-title::text").get()
+
+    timestamp = selector.css(
+        ".tec--timestamp__item time::attr(datetime)"
+    ).get()
+
+    writer = selector.css(".z--font-bold *::text").get()
+    if writer:
+        writer = writer.strip()
+
+    shares = selector.css(".tec--toolbar__item::text").get()
+    if shares:
+        shares = int(shares.split()[0])
+    else:
+        shares = 0
+
+    comments = selector.css("#js-comments-btn::attr(data-count)").get()
+
+    summary = "".join(
+        selector.css(".tec--article__body p:nth-child(1) *::text").getall()
+    )
+
+    categories = [
+        category.strip()
+        for category in selector.css("#js-categories a::text").getall()
+    ]
+
+    sources = [
+        source.strip()
+        for source in selector.css(
+            ".z--mb-16 .tec--badge::text"
+        ).getall()
+    ]
+
+    data = {
+        "url": url,
+        "title": title,
+        "timestamp": timestamp,
+        "writer": writer,
+        "shares_count": shares,
+        "comments_count": int(comments),
+        "summary": summary,
+        "categories": categories,
+        "sources": sources,
+    }
+
+    return data
 
 
 # Requisito 5
