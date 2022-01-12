208060353ae0cfb2fd6e29da3f94ef62ac654f6a
thalysmcarrara <thalysmatiascarrara@gmail.com>
2022-01-12 15:59:45 -0400

    requirement 4

--- tech_news/scraper.py
+++ tech_news/scraper.py
@@ -2,6 +2,26 @@ import requests
 import time
 from parsel import Selector
 
+URL_NEXT_PAGE_SELECTOR = ".tec--list a.tec--btn::attr(href)"
+
+AUTHOR_SELECTOR = ".z--font-bold ::text"
+
+URL_NEWS_SELECTOR = "head > link[rel=canonical]::attr(href)"
+
+TITLE_SELECTOR = "#js-article-title::text"
+
+TIMESTAMP_SELECTOR = "#js-article-date::attr(datetime)"
+
+SOURCES_SELECTOR = ".z--mb-16 .tec--badge::text"
+
+CATEGORIES_SELECTOR = "#js-categories a::text"
+
+SHARES_COUNT_SELECTOR = "#js-author-bar > nav > div:nth-child(1)::text"
+
+COMMENTS_COUNT_SELECTOR = "#js-comments-btn::attr(data-count)"
+
+SUMMARY_SELECTOR = ".tec--article__body p:first_child *::text"
+
 # Requisito 1
 
 
@@ -34,15 +54,60 @@ def scrape_novidades(html_content):
 
 def scrape_next_page_link(html_content):
     selector = Selector(text=html_content)
-    url = selector.css(".tec--list a.tec--btn::attr(href)").get()
+    url = selector.css(URL_NEXT_PAGE_SELECTOR).get()
     return url
 
 
 # Requisito 4
 def scrape_noticia(html_content):
-    """Seu código deve vir aqui"""
+    selector = Selector(text=html_content)
 
+    url = selector.css(URL_NEWS_SELECTOR).get()
 
+    title = selector.css(TITLE_SELECTOR).get()
+
+    timestamp = selector.css(TIMESTAMP_SELECTOR).get()
+
+    writer = selector.css(AUTHOR_SELECTOR).get()
+
+    if writer:
+        writer = writer.strip()
+    else:
+        writer = None
+
+    sources = [
+        source.strip() for source in selector.css(SOURCES_SELECTOR).getall()
+        ]
+
+    categories = [
+        category.strip() for category in
+        selector.css(CATEGORIES_SELECTOR).getall()
+        ]
+
+    shares_count = selector.css(SHARES_COUNT_SELECTOR).get()
+
+    if shares_count:
+        shares_count = shares_count.split()[0]
+    else:
+        shares_count = 0
+
+    comments_count = selector.css(COMMENTS_COUNT_SELECTOR).get()
+
+    summary = "".join(selector.css(SUMMARY_SELECTOR).getall())
+
+    return {
+        "url": url,
+        "title": title,
+        "timestamp": timestamp,
+        "writer": writer,
+        "sources": sources,
+        "categories": categories,
+        "shares_count": int(shares_count),
+        "comments_count": int(comments_count),
+        "summary": summary
+    }
 # Requisito 5
+
+
 def get_tech_news(amount):
     """Seu código deve vir aqui"""
