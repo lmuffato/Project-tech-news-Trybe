c7bab7ae789cc193846591052e2db194989abc21
Renan Oliveira <oliveira.renan.rg@gmail.com>
2022-01-12 09:00:46 -0300

    feat: start third requirement

--- tech_news/scraper.py
+++ tech_news/scraper.py
@@ -29,7 +29,14 @@ def scrape_novidades(html_content):
 
 # Requisito 3
 def scrape_next_page_link(html_content):
-    """Seu código deve vir aqui"""
+    selector = Selector(html_content)
+    next_page_link = selector.css(
+        ".tec--list a.tec--btn::attr(href)"
+    ).get()
+    if next_page_link:
+        return next_page_link
+    else:
+        return None
 
 
 # Requisito 4
