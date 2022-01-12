a7566b5bcaf1a220fca8f1dafa1ef388b9cc8ac0
Lucas André <lucas.cancado89@gmail.com>
2022-01-12 00:13:44 -0300

    req4

--- tech_news/scraper.py
+++ tech_news/scraper.py
@@ -1,6 +1,7 @@
 from parsel import Selector
 import requests
 import time
+import re
 
 
 # Requisito 1
@@ -33,7 +34,43 @@ def scrape_next_page_link(html_content):
 
 # Requisito 4
 def scrape_noticia(html_content):
-    """Seu código deve vir aqui"""
+    selector = Selector(text=html_content)
+    url = selector.xpath('//link[contains(@rel, "canonical")]/@href').get()
+    writer = selector.css(".z--font-bold ::text").get()
+    if writer:
+        writer = writer.strip()
+    else:
+        writer = None
+
+    shares_str = selector.css("#js-author-bar nav div::text").get()
+    if shares_str:
+        shares_num = int(re.findall(r"\d+", shares_str)[0])
+    else:
+        shares_num = 0
+
+    comments = selector.css("#js-comments-btn::attr(data-count)").get()
+
+    sources = []
+    for item in selector.css(".z--mb-16 .tec--badge::text").getall():
+        sources.append(item.strip())
+
+    categories = []
+    for i in selector.css("#js-categories a::text").getall():
+        categories.append(i.strip())
+
+    s = selector.css(".tec--article__body p:first-child *::text").getall()
+    response = {
+        "url": url,
+        "title": selector.css(".tec--article__header__title::text").get(),
+        "timestamp": selector.css("time::attr(datetime)").get(),
+        "writer": writer,
+        "shares_count": shares_num,
+        "comments_count": int(comments),
+        "summary": "".join(s),
+        "sources": sources,
+        "categories": categories
+    }
+    return response
 
 
 # Requisito 5
