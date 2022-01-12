417bbf5cf48a19729c3399aad5012d969e30f5aa
hernanne <hernannedomiciano@gmail.com>
2022-01-12 16:39:26 -0300

    feat: req04

--- tech_news/scraper.py
+++ tech_news/scraper.py
@@ -40,6 +40,55 @@ def scrape_next_page_link(html_content):
 # Requisito 4
 def scrape_noticia(html_content):
     """Seu código deve vir aqui"""
+    selector = Selector(html_content)
+
+    url = selector.css('head link[rel=canonical]::attr(href)').get()
+
+    title = selector.css('h1.tec--article__header__title::text').get()
+
+    datetime = selector.css(
+        'div.tec--timestamp__item time::attr(datetime)'
+    ).get()
+
+    writer = selector.css('.z--font-bold *::text').get()
+    if writer:
+        writer = writer.strip()
+
+    share_count = selector.css('div.tec--toolbar__item::text').get()
+    if share_count:
+        share_count = int(share_count.strip()[0])
+    else:
+        share_count = 0
+    comment_count = selector.css('#js-comments-btn::attr(data-count)').get()
+
+    summary = ''.join(selector.css(
+        'div.tec--article__body p:nth-child(1) *::text'
+    ).getall())
+
+    sources = selector.css('div.z--mb-16 .tec--badge::text').getall()
+    format_sources = [
+        i.strip()
+        for i in sources
+    ]
+
+    categories = selector.css('#js-categories a *::text').getall()
+    format_category = [
+        i.strip()
+        for i in categories
+    ]
+
+    body_notice = {
+        "url": url,
+        "title": title,
+        "timestamp": datetime,
+        "writer": writer,
+        "shares_count": share_count,
+        "comments_count": int(comment_count),
+        "summary": summary,
+        "sources": format_sources,
+        "categories": format_category,
+    }
+    return body_notice
 
 
 # Requisito 5
