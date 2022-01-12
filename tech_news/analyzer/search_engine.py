36dcf993332b548fdb8a80bb750c2ab9cddb64fd
Jodiel Briesemeister <jodielll29@gmail.com>
2022-01-12 19:01:36 -0300

    feat(req6): add search_by_title

--- tech_news/analyzer/search_engine.py
+++ tech_news/analyzer/search_engine.py
@@ -1,6 +1,14 @@
+from tech_news.database import search_news
+
+
 # Requisito 6
 def search_by_title(title):
-    """Seu código deve vir aqui"""
+
+    query = {"title": {"$regex": title, "$options": 'i'}}
+    # Case-insensitive(option i): https://kb.objectrocket.com/mongo-db/how-to-query-mongodb-documents-with-regex-in-python-362#create+a+case-insensitive+%24regex+query+in+pymongo+using+%24options
+    search_result = search_news(query)
+    result = [(news["title"], news["url"]) for news in search_result]
+    return result
 
 
 # Requisito 7
