dbfa22e14cbde1554f2d3742baecd3b66d7984ab
Hudson Carlos <hc88806@gmail.com>
2022-01-11 20:33:43 -0400

    fuction  scrape_next_page_link done

--- tech_news/scraper.py
+++ tech_news/scraper.py
@@ -32,7 +32,13 @@ def scrape_novidades(html_content):
 
 # Requisito 3
 def scrape_next_page_link(html_content):
-    """Seu código deve vir aqui"""
+    selector = Selector(text=html_content)
+
+    link = selector.css(
+        ".z--row a.z--mx-auto::attr(href)"
+    ).get()
+
+    return link
 
 
 # Requisito 4
