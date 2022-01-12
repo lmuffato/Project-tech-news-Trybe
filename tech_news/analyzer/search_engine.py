2f1960f68e7df29c43eac123759011584341aed8
thalysmcarrara <thalysmatiascarrara@gmail.com>
2022-01-12 19:27:30 -0400

    requirement 6

--- tech_news/analyzer/search_engine.py
+++ tech_news/analyzer/search_engine.py
@@ -1,6 +1,17 @@
+from tech_news.database import search_news
+
+
 # Requisito 6
 def search_by_title(title):
-    """Seu código deve vir aqui"""
+    query = {
+        "title": {
+            "$regex": title,
+            "$options": "i"
+        }
+    }
+    news = search_news(query)
+
+    return [(new["title"], new["url"]) for new in news]
 
 
 # Requisito 7
