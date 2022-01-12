f650cb6381c301877ec3396de93876fe9c0f2696
Rodrigo Rech <rodrigorech92@gmail.com>
2022-01-12 18:06:33 -0300

    Feat: scrape_next_page_link function implemented

--- tech_news/scraper.py
+++ tech_news/scraper.py
@@ -26,7 +26,9 @@ def scrape_novidades(html_content):
 
 # Requisito 3
 def scrape_next_page_link(html_content):
-    """Seu código deve vir aqui"""
+    selector = parsel.Selector(html_content)
+    next_page_link = selector.css("a.tec--btn::attr(href)").get()
+    return next_page_link
 
 
 # Requisito 4
