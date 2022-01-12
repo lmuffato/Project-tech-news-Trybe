18b85431bf59a5ab20b87d81f0f1bf5e8b1511de
Gui-lira <gui.lira.martins@gmail.com>
2022-01-12 13:33:56 -0300

    5

--- tech_news/scraper.py
+++ tech_news/scraper.py
@@ -2,6 +2,7 @@ from requests import get
 from time import sleep
 from bs4 import BeautifulSoup
 import requests
+from tech_news.database import create_news
 
 
 def fetch(url):
@@ -42,24 +43,38 @@ def scrape_next_page_link(html_content):
 # Requisito 4
 def scrape_noticia(html_content):
     readed = BeautifulSoup(html_content)
-    shareCount = readed.find('button', attrs={'id': 'js-comments-btn'})
+    divs = readed.find_all('div')
+    mapedDivs = list(map(lambda div: div.get_text(), divs))
+    filteredDivs = list(filter(lambda div: 'Compartilharam' in div, mapedDivs)) 
+    shareCount = 0
+    if len(filteredDivs) > 0:
+        shareCount = filteredDivs[-1].strip().split(' ')[0]
     comment = readed.find('button', attrs={'id': 'js-comments-btn'})
     rawSources = readed.find_all('a', attrs={'rel': 'noopener nofollow'})
     categories = readed.find_all('a', 'tec--badge tec--badge--primary')
     refindedSources = list(map(lambda a: a.get_text().strip(), rawSources))
-    filterdeSources = list(filter(lambda str: len(str) >= 2, refindedSources))
+
+    def filterDef(str):
+        v1 = len(str) >= 2 and 'preço' not in str
+        digit = str.replace(',', '', 1).replace('.', '', 1)
+        v2 = not digit.isdigit() and '$' not in str
+        if not v1 or not v2:
+            return False
+        return True
+    filterdeSources = list(filter(filterDef, refindedSources))
     url = readed.find('meta', attrs={'property': 'og:url'}).get('content')
     writers = list(filter(lambda a: 'autor' in a.get('href'),
                                     readed.find_all('a')))
     filteredWirters = list(filter(lambda a: len(a.get_text()) > 3, writers))
     filterTecMundo = list(filter(lambda txt: 'tec_mundo' not in txt,
                                              filterdeSources))
-    summary = readed.find('meta', attrs={'name': 'description'}).get('content')
+    article = readed.find('div', 'tec--article__body')
+    summary = article.find('p').get_text()
     returnDict = {
         'timestamp': readed.find('time').get('datetime'),
         'url': url,
         'title': readed.find('h1').get_text(),
-        'shares_count': int(shareCount.get('data-count')),
+        'shares_count': int(shareCount),
         'comments_count': int(comment.get('data-count')),
         'summary': summary,
         'sources': filterTecMundo,
@@ -74,4 +89,21 @@ def scrape_noticia(html_content):
 
 # Requisito 5
 def get_tech_news(amount):
-    """Seu código deve vir aqui"""
+    newsList = []
+    baseUrl = 'https://www.tecmundo.com.br/novidades'
+    nextLinks = ''
+    while len(newsList) < amount:
+        newsLink = []
+        if len(nextLinks) == 0:
+            newsLink = scrape_novidades(fetch(baseUrl))
+            nextLinks = scrape_next_page_link(fetch(baseUrl))
+        else:
+            newsLink = scrape_novidades(fetch(nextLinks))
+            nextLinks = scrape_next_page_link(fetch(nextLinks))
+        for link in newsLink:
+            if len(newsList) < amount:
+                newsList.append(scrape_noticia(fetch(link)))
+    create_news(newsList)
+    print(len(newsList))
+    print(newsList[0])
+    return newsList
