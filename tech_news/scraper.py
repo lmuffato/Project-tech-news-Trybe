19641ecade8898477cc3cf3fbad5c3f817b27a75
Lucas Machado <lucas.almeida.machado92@gmail.com>
2022-01-11 22:07:59 -0300

    Requisito 1 - criacao funcao fetch

--- tech_news/scraper.py
+++ tech_news/scraper.py
@@ -1,6 +1,17 @@
+import requests
+import time
+
+
 # Requisito 1
 def fetch(url):
     """Seu código deve vir aqui - Iniciando - commit 1"""
+    try:
+        response = requests.get(url, timeout=3)
+        time.sleep(1)
+        if response.status_code == 200:
+            return response.text
+    except requests.Timeout:
+        return None
 
 
 # Requisito 2
