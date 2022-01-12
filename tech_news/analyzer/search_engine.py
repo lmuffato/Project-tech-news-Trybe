0598eba6143026d2a9533c34b8f4c7de82037abf
Marcelo Maurício <mmauricio.design@gmail.com>
2022-01-12 19:23:57 -0300

    Feat 6: created a function where return a tuple by title

--- tech_news/analyzer/search_engine.py
+++ tech_news/analyzer/search_engine.py
@@ -1,5 +1,6 @@
 # from pymongo import MongoClient
 from tech_news.database import db, client
+import re
 
 
 # Requisito 6
@@ -7,13 +8,16 @@ def search_by_title(title):
     title_list = []
 
     with client:
-        for new in db.news.find({"title": title}):
+        for new in db.news.find({"title": re.compile(title, re.IGNORECASE)}):
             print(title)
             title_list.append((new["title"], new["url"]))
 
     print("Lista final", title_list)
 
     return title_list
+
+# Conseguir deixar ele como case insensitive com o link:
+# https://stackoverflow.com/questions/6266555/querying-mongodb-via-pymongo-in-case-insensitive-efficiently
 
 
 # Requisito 7
