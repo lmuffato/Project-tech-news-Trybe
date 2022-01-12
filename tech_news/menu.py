497f3973691f3aee7f466fd23681a7e9348988aa
Viegas <mvrdgs@gmail.com>
2022-01-12 18:34:07 -0300

    requisito 12

--- tech_news/menu.py
+++ tech_news/menu.py
@@ -1,3 +1,15 @@
 # Requisito 12
 def analyzer_menu():
-    """Seu código deve vir aqui"""
+    option = input(
+        """Selecione uma das opções a seguir:
+ 0 - Popular o banco com notícias;
+ 1 - Buscar notícias por título;
+ 2 - Buscar notícias por data;
+ 3 - Buscar notícias por fonte;
+ 4 - Buscar notícias por categoria;
+ 5 - Listar top 5 notícias;
+ 6 - Listar top 5 categorias;
+ 7 - Sair."""
+    )
+
+    return option
