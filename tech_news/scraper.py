6b98725c2d0f46145038ec7a17702d8c7e447f9f
Rodrigo Rech <rodrigorech92@gmail.com>
2022-01-12 17:48:17 -0300

    Feat: scrape_novidades function implemented

--- tech_news/scraper.py
+++ tech_news/scraper.py
@@ -1,5 +1,6 @@
 import requests
 import time
+import parsel
 # Requisito 1
 
 
@@ -16,7 +17,11 @@ def fetch(url):
 
 # Requisito 2
 def scrape_novidades(html_content):
-    """Seu código deve vir aqui"""
+    selector = parsel.Selector(html_content)
+    links = selector.css(
+        "h3.tec--card__title a.tec--card__title__link::attr(href)"
+        ).getall()
+    return links
 
 
 # Requisito 3
