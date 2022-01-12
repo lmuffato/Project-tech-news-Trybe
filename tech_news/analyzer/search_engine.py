14b243fa9b64ea517103e5af7d21dc5522217fcf
Jodiel Briesemeister <jodielll29@gmail.com>
2022-01-12 19:51:27 -0300

    feat(req7): fix flake8, add search_by_date, add formated_search

--- tech_news/analyzer/search_engine.py
+++ tech_news/analyzer/search_engine.py
@@ -1,19 +1,36 @@
 from tech_news.database import search_news
+from datetime import datetime
+
+
+def formated_search(query):
+    search_result = search_news(query)
+    result = [(news["title"], news["url"]) for news in search_result]
+    return result
 
 
 # Requisito 6
 def search_by_title(title):
 
     query = {"title": {"$regex": title, "$options": 'i'}}
-    # Case-insensitive(option i): https://kb.objectrocket.com/mongo-db/how-to-query-mongodb-documents-with-regex-in-python-362#create+a+case-insensitive+%24regex+query+in+pymongo+using+%24options
-    search_result = search_news(query)
-    result = [(news["title"], news["url"]) for news in search_result]
+    # Case-insensitive(option i):
+    # https://kb.objectrocket.com/mongo-db/how-to-query-mongodb-documents-with-regex-in-python-362#create+a+case-insensitive+%24regex+query+in+pymongo+using+%24options
+    result = formated_search(query)
     return result
 
 
 # Requisito 7
 def search_by_date(date):
-    """Seu código deve vir aqui"""
+    try:
+        datetime.strptime(date, "%Y-%m-%d")
+        # datetime strptime:
+        # https://www.programiz.com/python-programming/datetime/strptime
+        query = {"timestamp": {
+            '$regex': date
+        }}
+        result = formated_search(query)
+        return result
+    except ValueError:
+        raise ValueError("Data inválida")
 
 
 # Requisito 8
