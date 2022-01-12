234fb62853d761b2f86c7f84b9f9ebddfacc7c79
AndreBarroso <andre_barroso17@hotmail.com>
2022-01-12 19:45:47 -0300

    feat: requisito 2

--- tech_news/scraper.py
+++ tech_news/scraper.py
@@ -1,5 +1,6 @@
 import requests
 import time
+from parsel import Selector
 
 
 # Requisito 1
@@ -19,6 +20,12 @@ def fetch(url):
 # Requisito 2
 def scrape_novidades(html_content):
     """Seu código deve vir aqui"""
+    selector = Selector(html_content)
+    links_list = []
+    for news_link in selector.css("h3.tec--card__title"):
+        link = news_link.css("a.tec--card__title__link::attr(href)").get()
+        links_list.append(link)
+    return links_list
 
 
 # Requisito 3
