5695abfce0cea9f83ebb2d9d77918b5bf756f933
nascjoao <oi@joaonasc.dev>
2022-01-12 18:07:14 -0300

    feat: search news by title

--- tech_news/analyzer/search_engine.py
+++ tech_news/analyzer/search_engine.py
@@ -1,6 +1,16 @@
+from tech_news.database import search_news
+import re
+
+
 # Requisito 6
 def search_by_title(title):
-    """Seu código deve vir aqui"""
+    # https://stackoverflow.com/questions/3483318/performing-regex-queries-with-pymongo
+    regx = re.compile(f".*{title}.*", re.IGNORECASE)
+    found_news = search_news({"title": regx})
+    list = []
+    for news in found_news:
+        list.append((news["title"], news["url"]))
+    return list
 
 
 # Requisito 7
