2826df92569583fc562e11ff498fc39c8bee39ba
Marcus Hüsemann <mchusemann@gmail.com>
2022-01-13 11:36:23 -0300

    feat(search_engine.py): adiciona requisito 6 que busca no dict de noticias, as noticias por data

--- tech_news/analyzer/search_engine.py
+++ tech_news/analyzer/search_engine.py
@@ -1,32 +1,48 @@
+from datetime import datetime
 from tech_news.database import search_news
 import re
 
 
-def tuple_of_title_url_from_dict(news, title):
+def tuple_from_dict(news, search_param, search_param_str):
     return [tuple((new['title'], new['url']))
             for new in news
-            if new['title'].lower() == title.lower()]
+            if search_param.lower() in new[search_param_str].lower()]
+
 
 def create_new_regex_ignore_case(value):
     return re.compile(f'^{value}', re.IGNORECASE)
 
 
+def exists(value):
+    if(not value):
+        return []
+    else:
+        return value
+
+
 def search_by_title(title):
     reg = create_new_regex_ignore_case(title)
     news = search_news({'title': reg})
-    titles = tuple_of_title_url_from_dict(news, title)
+    titles = tuple_from_dict(news, title, 'title')
 
-    if(not titles):
-        return []
-    return titles
+    return exists(titles)
 
 
-# Requisito 7
+def validate_date(date_str):
+    try:
+        date_format = '%Y-%m-%d'
+        datetime.strptime(date_str, date_format)
+    except ValueError:
+        raise ValueError('Data inválida')
+
+
 def search_by_date(date):
-    """Seu código deve vir aqui"""
+    validate_date(date)
+    news = search_news({'timestamp': {'$regex': date}})
+    dates = tuple_from_dict(news, date, 'timestamp')
+    return exists(dates)
 
 
-# Requisito 8
 def search_by_source(source):
     """Seu código deve vir aqui"""
 
