ad701f5f9160ac6a06c66f7ae3cc1b340f265056
Marcus Hüsemann <mchusemann@gmail.com>
2022-01-13 10:25:50 -0300

    feat(search_engine.py): adiciona requisito 6 que busca no dict de noticias, as noticias por titulo

--- tech_news/analyzer/search_engine.py
+++ tech_news/analyzer/search_engine.py
@@ -1,6 +1,24 @@
-# Requisito 6
+from tech_news.database import search_news
+import re
+
+
+def tuple_of_title_url_from_dict(news, title):
+    return [tuple((new['title'], new['url']))
+            for new in news
+            if new['title'].lower() == title.lower()]
+
+def create_new_regex_ignore_case(value):
+    return re.compile(f'^{value}', re.IGNORECASE)
+
+
 def search_by_title(title):
-    """Seu código deve vir aqui"""
+    reg = create_new_regex_ignore_case(title)
+    news = search_news({'title': reg})
+    titles = tuple_of_title_url_from_dict(news, title)
+
+    if(not titles):
+        return []
+    return titles
 
 
 # Requisito 7
