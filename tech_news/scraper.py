633a30d95f9912f3f952dca6ae519b553b51ce70
Michael <michaelpetterson06.com.br>
2022-01-13 12:49:50 -0300

    req 02

--- tech_news/scraper.py
+++ tech_news/scraper.py
@@ -1,5 +1,6 @@
 import requests
 import time
+from parsel import Selector
 
 
 # Requisito 1
@@ -17,7 +18,9 @@ def fetch(url):
 
 # Requisito 2
 def scrape_novidades(html_content):
-    """Seu código deve vir aqui"""
+    selector = Selector(text=html_content)
+    news_selector = ".tec--list .tec--card__title__link::attr(href)"
+    return selector.css(news_selector).getall()
 
 
 # Requisito 3
