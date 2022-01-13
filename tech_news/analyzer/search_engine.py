aad435320ae7762ff4f79bfd3376f2b32b0e2380
thalysmcarrara <thalysmatiascarrara@gmail.com>
2022-01-13 10:21:02 -0400

    requirement 7

--- tech_news/analyzer/search_engine.py
+++ tech_news/analyzer/search_engine.py
@@ -1,4 +1,5 @@
 from tech_news.database import search_news
+from datetime import datetime
 
 
 # Requisito 6
@@ -16,7 +17,19 @@ def search_by_title(title):
 
 # Requisito 7
 def search_by_date(date):
-    """Seu código deve vir aqui"""
+    try:
+        datetime.strptime(date, '%Y-%m-%d')
+    except ValueError:
+        raise ValueError('Data inválida')
+
+    query = {
+        "timestamp": {
+            "$regex": date
+            }
+        }
+    news = search_news(query)
+
+    return [(new["title"], new["url"]) for new in news]
 
 
 # Requisito 8
