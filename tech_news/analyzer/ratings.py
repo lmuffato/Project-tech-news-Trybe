308d7725d917f9bdbcbeb61db804ef2c02687f49
Bruno Mendes <mendessbruno@gmail.com>
2022-01-13 20:16:06 -0300

    resolvido desafio 10

--- tech_news/analyzer/ratings.py
+++ tech_news/analyzer/ratings.py
@@ -1,5 +1,24 @@
+from tech_news.database import find_news
+
+
 # Requisito 10
 def top_5_news():
+    news_list = find_news()
+    result = []
+    if news_list:
+        for news in news_list:
+            popularidade = 0
+            popularidade += news["shares_count"]
+            popularidade += news["comments_count"]
+            news["popularidade"] = popularidade
+    sorted_news = sorted(
+        news_list, key=lambda i: (i['popularidade'], i['title']), reverse=True)
+    index = 0
+    for news in sorted_news:
+        if index < 5:
+            result.append((news["title"], news["url"]))
+            index += 1
+    return result
     """Seu código deve vir aqui"""
 
 
