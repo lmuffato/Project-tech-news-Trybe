d3e3cd768c2e063c17a6e937807ccf8f746547d4
Bruno Mendes <mendessbruno@gmail.com>
2022-01-13 19:41:46 -0300

    resolvidos requisitos 8 e 9

--- tech_news/analyzer/search_engine.py
+++ tech_news/analyzer/search_engine.py
@@ -30,9 +30,21 @@ def search_by_date(date):
 
 # Requisito 8
 def search_by_source(source):
+    news_list = search_news({"sources": {"$regex": source, "$options": "i"}})
+    result = []
+    if news_list:
+        for news in news_list:
+            result.append((news["title"], news["url"]))
+    return result
     """Seu código deve vir aqui"""
 
 
 # Requisito 9
 def search_by_category(category):
+    news_list = search_news({"categories": {"$regex": category, "$options": "i"}})
+    result = []
+    if news_list:
+        for news in news_list:
+            result.append((news["title"], news["url"]))
+    return result
     """Seu código deve vir aqui"""
