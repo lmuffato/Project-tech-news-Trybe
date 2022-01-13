aa8ad0e7024bdaf998baeec6dfba52c036da27e5
Rodrigo Rech <rodrigorech92@gmail.com>
2022-01-13 16:14:13 -0300

    Feat: scrape_noticia function implemented

--- tech_news/scraper.py
+++ tech_news/scraper.py
@@ -33,7 +33,40 @@ def scrape_next_page_link(html_content):
 
 # Requisito 4
 def scrape_noticia(html_content):
-    """Seu código deve vir aqui"""
+    selector = parsel.Selector(html_content)
+
+    writer_selector = selector.css("a.tec--author__info__link::text").get()
+    writer = writer_selector.strip() if writer_selector else None
+
+    shares = selector.css(".tec--toolbar__item::text").re_first(r"\d+")
+    shares_count = int(shares) if shares else 0
+
+    comments = selector.css("#js-comments-btn::text").re_first(r"\d+")
+    comments_count = int(comments) if comments else 0
+
+    summary_selector = selector.css(
+      ".tec--article__body > p:first_child *::text").getall()
+    summary = "".join(summary_selector).strip()
+
+    sources_selector = selector.css("div.z--mb-16 a::text").getall()
+    sources = [source.strip() for source in sources_selector]
+
+    categories_selector = selector.css("#js-categories a::text").getall()
+    categories = [category.strip() for category in categories_selector]
+
+    response = {
+        "url": selector.css("link[rel=amphtml]::attr(href)").get(),
+        "title": selector.css("#js-article-title ::text").get(),
+        "timestamp": selector.css("#js-article-date::attr(datetime)").get(),
+        "writer": writer,
+        "shares_count": shares_count,
+        "comments_count": comments_count,
+        "summary": summary,
+        "sources": sources,
+        "categories": categories
+
+    }
+    return response
 
 
 # Requisito 5
