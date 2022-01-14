952bfaca2d0c4444ddb693fba57b1bc6f70ff59f
Hudson Carlos <hc88806@gmail.com>
2022-01-14 11:50:12 -0400

    function search_by_date done

--- tech_news/analyzer/search_engine.py
+++ tech_news/analyzer/search_engine.py
@@ -1,4 +1,5 @@
 from tech_news.database import find_news
+import datetime
 
 
 # Requisito 6
@@ -8,13 +9,33 @@ def search_by_title(title):
     list_duple = []
     for noticie in find_title:
         list_duple.append((noticie["title"], noticie["url"]))
-    print(list_duple)
+
     return list_duple
 
 
 # Requisito 7
+def extend_search_by_date(date):
+    list_mongo = find_news()
+    find_data = []
+    for obj in list_mongo:
+        format_date = obj["timestamp"]
+        format_date = format_date.split("T")
+        if format_date[0] == date:
+            find_data.append(obj)
+
+    list_duple = []
+    for noticie in find_data:
+        list_duple.append((noticie["title"], noticie["url"]))
+    return list_duple
+
+
 def search_by_date(date):
-    """Seu código deve vir aqui"""
+    try:
+        datetime.datetime.strptime(date, "%Y-%m-%d")
+    except ValueError:
+        raise ValueError("Data inválida")
+    else:
+        return extend_search_by_date(date)
 
 
 # Requisito 8
