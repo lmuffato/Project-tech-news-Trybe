c1f0b149adff8bf2806948646d5c5ce1193f030b
Lucas Machado <lucas.almeida.machado92@gmail.com>
2022-01-14 11:42:39 -0300

    Req4: Retorna noticia - Refatorado - Most difficult - 3 days to do

--- tech_news/scraper.py
+++ tech_news/scraper.py
@@ -38,51 +38,59 @@ def scrape_next_page_link(html_content):
         return None
 
 
+def find_writer(selector):
+    aux_writer = selector.css(".z--font-bold *::text").get()
+    if aux_writer:
+        return aux_writer.strip()
+    else:
+        return None
+
+
+def find_shares_count(selector):
+    aux_shares_count = selector.css(".tec--toolbar__item::text").get()
+    if aux_shares_count is None:
+        return 0
+    else:
+        return aux_shares_count[:2]
+
+
 # Requisito 4
 def scrape_noticia(html_content):
     """Seu código deve vir aqui"""
     selector = Selector(html_content)
-    # print(selector.css("link::attr(href)").getall())
+
     aux_selector = selector.css("link")
     for link in aux_selector:
         if link.css("link[rel=canonical]").get():
             url = link.css("link::attr(href)").get()
-    print(url)
+
     title = selector.css("#js-article-title::text").get()
-    print(title)
+
     timestamp = selector.css("time::attr(datetime)").get()
-    print(timestamp)
-    aux_writer = selector.css(".z--font-bold ::text").get()
-    if aux_writer:
-        writer = aux_writer.strip()
-    else:
-        writer = None
-    print(writer)
-    aux_shares_count = selector.css(".tec--toolbar__item::text").get()
-    if aux_shares_count is None:
-        shares_count = 0
-    else:
-        shares_count = aux_shares_count[:2]
-    print(shares_count)
+
+    writer = find_writer(selector)
+
+    shares_count = find_shares_count(selector)
+
     comments_count = selector.css("button::attr(data-count)").get()
-    print(comments_count)
+
     # https://devhints.io/css
     # https://pt.stackoverflow.com/questions/324979/como-concatenar-itens-de-uma-lista-em-python
+
     aux_summary = selector.css(
         "div.tec--article__body > p:first-child *::text"
     )
     summary = ''.join(aux_summary.getall())
-    print(summary)
+
     sources = []
     for source in selector.css(".z--mb-16 .tec--badge::text").getall():
         sources.append(source.strip())
-    print(sources)
+
     categories = []
     for category in selector.css(
         "#js-categories a::text"
     ).getall():
         categories.append(category.strip())
-    print(categories)
 
     return {
         "url": url,
@@ -95,7 +103,6 @@ def scrape_noticia(html_content):
         "sources": sources,
         "categories": categories
     }
-
 
 
 # Requisito 5
