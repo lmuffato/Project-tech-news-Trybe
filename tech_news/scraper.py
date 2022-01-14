b98a7285b7baad971019ab7c74c64d8992d81c5a
jonathan <jonathanfernandesjf98@gmail.com>
2022-01-14 20:01:34 -0300

    requisito 4 ainda nao passando

--- tech_news/scraper.py
+++ tech_news/scraper.py
@@ -29,8 +29,59 @@ def scrape_next_page_link(html_content):
 # Requisito 4
 def scrape_noticia(html_content):
     """Seu código deve vir aqui"""
+    # selector = parsel.Selector(html_content)
+    # all_links = selector.css(".tec--list__item  .tec--card__title__link::attr(href)").getall()
+    # for link in all_links:
+        # html_notice = requests.get(html_content)
+    # print(html_content)
+    notice_selector = parsel.Selector(html_content)
+    link = notice_selector.css("head link[rel=canonical]::attr(href)").get()
+    title = notice_selector.css(".tec--article__header__title::text").get()
+    timestamp = notice_selector.css("#js-article-date::attr(datetime)").get()
+    writer = (notice_selector.css(".tec--author__info__link::text").get() 
+    or notice_selector.css(".tec--timestamp__item.z--font-bold > a::text").get())
+    if writer != None:
+        writer = writer.strip()    
+    shares_count = notice_selector.css(".tec--toolbar__item::text").re_first(r"\d+")
+    comments_count = notice_selector.css("#js-comments-btn::text").re_first(r"\d+")
+    if comments_count == None:
+        comments_count = 0
+    else:
+        comments_count = int(comments_count)
+    if shares_count == None:
+        shares_count = 0
+    else:
+        shares_count = int(shares_count)
+    sources = notice_selector.css(".z--mb-16 .tec--badge::text").getall()
+    formated_sources = []
+    for source in sources:
+        formated_source = source.strip()
+        formated_sources.append(formated_source)
+    summary = notice_selector.css("div.tec--article__body p:nth-child(1) *::text").getall()
 
+    formated_summary = ''.join(summary)
+    categories = notice_selector.css("#js-categories a::text").getall()
+    formated_categories = []
+    for categorie in categories:
+        formated_categorie = categorie.strip(" ")
+        formated_categories.append(formated_categorie)
+    return {
+        "url": link,
+        "title": title,
+        "timestamp": timestamp,
+        "writer": writer,
+        "shares_count": shares_count,
+        "comments_count": comments_count,
+        "summary": formated_summary,
+        "sources": formated_sources,
+        "categories": formated_categories,
+    }
+
+# fonte de consulta para o método strip: https://www.tutorialspoint.com/python3/string_strip.htm
 
 # Requisito 5
 def get_tech_news(amount):
     """Seu código deve vir aqui"""
+
+    #js-main > div > article > div.tec--article__body-grid > div.z--pt-40.z--pb-24 > 
+    # div.z--flex.z--items-center > div.tec--timestamp.tec--timestamp--lg > 
