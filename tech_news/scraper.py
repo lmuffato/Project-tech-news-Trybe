81a9bc1e3ff6362491404b2dd649f936975c4dc1
Orlando Flores <orlando_flores@id.uff.br>
2022-01-15 15:56:08 -0300

    feat: requisito 4

--- tech_news/scraper.py
+++ tech_news/scraper.py
@@ -36,7 +36,37 @@ def scrape_next_page_link(html_content):
 
 # Requisito 4
 def scrape_noticia(html_content):
-    """Seu código deve vir aqui"""
+    page = Selector(html_content)
+
+    url = page.css("meta[property='og:url']::attr(content)").get()
+    title = page.css(".tec--article__header__title::text").get()
+    timestamp = page.css("#js-article-date::attr(datetime)").get()
+    writer = page.css(".z--font-bold *::text").get()
+    shares_count = page.css(".tec--toolbar__item::text").get()
+    comments_count = page.css("#js-comments-btn::attr(data-count)").get()
+
+    summary_css_selector = ".tec--article__body p:first-child *::text"
+    summary = "".join(page.css(summary_css_selector).getall()).strip()
+
+    sources_css_selector = ".z--mb-16 a.tec--badge::text"
+    sources_list = page.css(sources_css_selector).getall()
+    sources = [source.strip() for source in sources_list]
+
+    categories_css_selector = "#js-categories a::text"
+    categories_list = page.css(categories_css_selector).getall()
+    categories = [category.strip() for category in categories_list]
+
+    return {
+        "url": url,
+        "title": title,
+        "timestamp": timestamp,
+        "writer": writer.strip() if writer else None,
+        "shares_count": int(shares_count.split()[0]) if shares_count else 0,
+        "comments_count": int(comments_count) if comments_count else 0,
+        "summary": summary,
+        "sources": sources,
+        "categories": categories,
+    }
 
 
 # Requisito 5
