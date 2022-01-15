331b2621b2d2ae91cb1859920be54c0fdfe2640e
Alexandre Oliveira <alexandremolive@gmail.com>
2022-01-15 16:27:50 -0300

    req 6 done

--- tech_news/analyzer/search_engine.py
+++ tech_news/analyzer/search_engine.py
@@ -1,6 +1,16 @@
+import re
+from tech_news.database import search_news
+
+
 # Requisito 6
 def search_by_title(title):
     """Seu código deve vir aqui"""
+    news_by_title = search_news({
+        "title": re.compile(title, re.IGNORECASE)})
+    if (len(news_by_title)):
+        return [(item["title"], item["url"]) for item in news_by_title]
+    else:
+        return []
 
 
 # Requisito 7
