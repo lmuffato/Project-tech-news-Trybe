355177c3dd19b353854e7baca9b390ee4bac9001
GuilhermeMD <guilherme.md@gmail.com>
2022-01-15 17:59:30 -0300

    req 03 - implementation

--- tech_news/scraper.py
+++ tech_news/scraper.py
@@ -26,7 +26,9 @@ def scrape_novidades(html_content):
 
 # Requisito 3
 def scrape_next_page_link(html_content):
-    """Seu código deve vir aqui"""
+    selector = Selector(text=html_content)
+    see_more_news_btn = selector.css(".tec--list a.tec--btn::attr(href)").get()
+    return see_more_news_btn
 
 
 # Requisito 4
