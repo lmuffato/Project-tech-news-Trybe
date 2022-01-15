cb2d216321307949cf9fcb0e879c0c10ff20d933
MaxRudim <max.hermsdorff@gmail.com>
2022-01-15 15:38:03 -0300

    Requisito 07

--- tech_news/analyzer/search_engine.py
+++ tech_news/analyzer/search_engine.py
@@ -1,4 +1,5 @@
-from tech_news.database import db
+from datetime import datetime
+from tech_news.database import search_news
 
 
 # Requisito 6
@@ -8,7 +9,7 @@ def search_by_title(title):
     # https://stackoverflow.com/questions/8246019/case-insensitive-search-in-mongo
 
     query = {"title": {"$regex": title, "$options": "i"}}
-    find_news = db.news.find(query)
+    find_news = search_news(query)
     data = []
 
     for new in find_news:
@@ -20,6 +21,18 @@ def search_by_title(title):
 # Requisito 7
 def search_by_date(date):
     """Seu código deve vir aqui"""
+    try:
+        datetime.strptime(date, "%Y-%m-%d")
+        query = {"timestamp": {"$regex": date, "$options": "i"}}
+        find_news = search_news(query)
+        data = []
+
+        for new in find_news:
+            data.append((new["title"], new["url"]))
+
+        return data
+    except ValueError:
+        raise ValueError("Data inválida")
 
 
 # Requisito 8
