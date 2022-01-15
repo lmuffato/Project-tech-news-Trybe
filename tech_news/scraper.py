36dff7660d432e64818449ab781bbf647c3d75df
Alexandre Oliveira <alexandremolive@gmail.com>
2022-01-15 13:06:34 -0300

    req 1 done

--- tech_news/scraper.py
+++ tech_news/scraper.py
@@ -1,6 +1,20 @@
+from parsel import Selector
+import requests
+import time
+
+
 # Requisito 1
 def fetch(url):
     """Seu código deve vir aqui"""
+    try:
+        time.sleep(1)
+        response = requests.get(url, timeout=3)
+        if (response.status_code == 200):
+            return response.text
+        else:
+            return None
+    except requests.ReadTimeout:
+        return None
 
 
 # Requisito 2
