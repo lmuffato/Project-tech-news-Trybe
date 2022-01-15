7020445852994641e91c67d5605ba93417f24747
Alexandre Oliveira <alexandremolive@gmail.com>
2022-01-15 14:30:57 -0300

    req 4 done

--- tech_news/scraper.py
+++ tech_news/scraper.py
@@ -42,6 +42,38 @@ def scrape_next_page_link(html_content):
 # Requisito 4
 def scrape_noticia(html_content):
     """Seu código deve vir aqui"""
+    selector = Selector(html_content)
+    url = selector.css("meta[property='og:url']::attr(content)").get()
+    title = selector.css("h1.tec--article__header__title::text").get()
+    timestamp = selector.css(
+        "div.tec--timestamp__item time::attr(datetime)").get()
+    writer = selector.css(
+        ".tec--author__info__link::text").get() or selector.css(
+        "div.tec--timestamp__item a::text").get() or selector.css(
+            "div.tec--author__info p.z--font-bold::text").get() or None
+    shares_count = selector.css(".tec--toolbar__item::text").get() or 0
+    comments_count = selector.css("#js-comments-btn::attr(data-count)").get()
+    summary = "".join(selector.css(
+        ".tec--article__body > p:first-child *::text"
+        ).getall())
+    sources = [item.strip() for item in selector.css(
+        "div.z--mb-16 div a::text"
+        ).getall()]
+    categories = [item.strip() for item in selector.css(
+        "#js-categories a::text"
+        ).getall()]
+
+    return {
+       "url": url,
+       "title": title,
+       "timestamp": timestamp,
+       "writer": writer.strip() if writer else None,
+       "shares_count": int(shares_count.split()[0]) if shares_count else 0,
+       "comments_count": int(comments_count) if comments_count else 0,
+       "summary": summary,
+       "sources": sources,
+       "categories": categories,
+    }
 
 
 # Requisito 5
