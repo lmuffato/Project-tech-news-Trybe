8391eb37915f2dcb7ce586c55c4f80a1e51b6e2e
AndreBarroso <andre_barroso17@hotmail.com>
2022-01-15 13:27:46 -0300

    feat: requisito 10

--- tech_news/analyzer/ratings.py
+++ tech_news/analyzer/ratings.py
@@ -1,6 +1,22 @@
+from ..database import find_news
+
+
 # Requisito 10
 def top_5_news():
     """Seu código deve vir aqui"""
+    data = find_news()
+    news = []
+
+    data.sort(
+        key=lambda item: (item["shares_count"] + item["comments_count"]),
+        reverse=True,
+    )
+
+    for item in data[:5]:
+        noticia = (item["title"], item["url"])
+        news.append(noticia)
+
+    return news
 
 
 # Requisito 11
