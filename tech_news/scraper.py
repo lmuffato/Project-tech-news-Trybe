b9d6816053cde083317ce228aaed0b6ae6979395
luanlsr <luanfswd@gmail.com>
2022-01-15 18:31:25 -0300

    req4 finalizado

--- tech_news/scraper.py
+++ tech_news/scraper.py
@@ -25,12 +25,47 @@ def scrape_novidades(html_content):
 
 # Requisito 3
 def scrape_next_page_link(html_content):
-    """Seu código deve vir aqui"""
+    selector = Selector(html_content)
+    next_page_link = selector.css("div.tec--list a.tec--btn::attr(href)").get()
+    if next_page_link:
+        return next_page_link
+    else:
+        return None
 
 
 # Requisito 4
 def scrape_noticia(html_content):
-    """Seu código deve vir aqui"""
+    page = Selector(html_content)
+
+    url = page.css("meta[property='og:url']::attr(content)").get()
+    title = page.css(".tec--article__header__title::text").get()
+    timestamp = page.css("#js-article-date::attr(datetime)").get()
+    writer = page.css(".z--font-bold *::text").get()
+    shares_count = page.css(".tec--toolbar__item::text").get()
+    comments_count = page.css("#js-comments-btn::attr(data-count)").get()
+
+    summary_selector = ".tec--article__body > p:first-child *::text"
+    summary = "".join(page.css(summary_selector).getall()).strip()
+
+    sources_selector = ".z--mb-16 a.tec--badge::text"
+    sources_list = page.css(sources_selector).getall()
+    sources = [source.strip() for source in sources_list]
+
+    categories_css_selector = "#js-categories a::text"
+    categories_list = page.css(categories_css_selector).getall()
+    categories = [category.strip() for category in categories_list]
+
+    return {
+        "url": url,
+        "title": title,
+        "timestamp": timestamp,
+        "writer": writer.strip() if writer else None,
+        "shares_count": int(shares_count.split()[0]) if shares_count else 0,
+        "comments_count": int(comments_count) if comments_count else 0,
+        "summary": summary,
+        "sources": sources,
+        "categories": categories,
+    }
 
 
 # Requisito 5
