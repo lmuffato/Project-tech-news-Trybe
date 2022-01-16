556e83bd5b50665ef4bedd28c71f420ab3bec191
Daniel Ribeiro <neto.daniribeiro@gmail.com>
2022-01-16 10:50:10 -0300

    Requisito 3: scrape_nest_page_link

--- tech_news/scraper.py
+++ tech_news/scraper.py
@@ -28,7 +28,13 @@ def scrape_novidades(html_content):
 
 # Requisito 3
 def scrape_next_page_link(html_content):
-    """Seu código deve vir aqui"""
+    selector = Selector(html_content)
+    link = selector.css("div.tec--list a.tec--btn::attr(href)").get()
+
+    if link:
+        return link
+    else:
+        return None
 
 
 # Requisito 4
