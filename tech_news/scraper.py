1c7db71ef401a105efe275c5ac22a1c994c5d848
NilsonRCS <nilson.ribeiro.souza3@gmail.com>
2022-01-16 16:29:47 -0300

    feat: requiremant:03

--- tech_news/scraper.py
+++ tech_news/scraper.py
@@ -27,7 +27,12 @@ def scrape_novidades(html_content):
 
 # Requisito 3
 def scrape_next_page_link(html_content):
-    """Seu código deve vir aqui"""
+    selector = parsel.Selector(html_content)
+    btn = selector.css("a.tec--btn::attr(href)").get()
+    if btn:
+        return btn
+    else:
+        return None
 
 
 # Requisito 4
