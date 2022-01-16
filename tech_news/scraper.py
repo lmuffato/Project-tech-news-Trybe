c4f052cca6f29bbc5e4d80e71cf2c834b46f7a7e
William <w.willers@outlook.com>
2022-01-16 16:16:23 -0400

    Req 4 e 5

--- tech_news/scraper.py
+++ tech_news/scraper.py
@@ -1,5 +1,6 @@
 from ratelimit import limits, sleep_and_retry
 from parsel import Selector
+from tech_news.database import create_news
 
 import requests
 
@@ -39,9 +40,57 @@ def scrape_next_page_link(html_content):
 
 # Requisito 4
 def scrape_noticia(html_content):
-    """Seu código deve vir aqui"""
+    selector = Selector(html_content)
+
+    url = selector.css("meta[property='og:url']::attr(content)").get()
+    title = selector.css(".tec--article__header__title::text").get()
+    timestamp = selector.css(
+        ".tec--timestamp__item time::attr(datetime)"
+    ).get()
+    author = selector.css(".z--font-bold *::text").get().strip()
+    shares_count = selector.css(".tec--toolbar__item::text").get()
+    comments_count = selector.css("#js-comments-btn::attr(data-count)").get()
+    summary_selector = ".tec--article__body > p:first-child *::text"
+
+    # https://www.w3schools.com/python/ref_string_strip.asp
+    summary = "".join(selector.css(summary_selector).getall()).strip()
+
+    # https://www.geeksforgeeks.org/scrapy-selectors/
+    selected_sources = ".z--mb-16 a.tec--badge::text"
+    sources_list = selector.css(selected_sources).getall()
+    sources = [source.strip() for source in sources_list]
+    selected_categories = "#js-categories a::text"
+    categories_list = selector.css(selected_categories).getall()
+    categories = [category.strip() for category in categories_list]
+
+    return {
+        "url": url,
+        "title": title,
+        "timestamp": timestamp,
+        "writer": author if author else None,
+        "shares_count": int(shares_count.split()[0]) if shares_count else 0,
+        "comments_count": int(comments_count) if comments_count else 0,
+        "summary": summary,
+        "sources": sources,
+        "categories": categories,
+    }
 
 
 # Requisito 5
 def get_tech_news(amount):
-    """Seu código deve vir aqui"""
+    content = fetch("https://www.tecmundo.com.br/novidades")
+    url = []
+    news_links = []
+    url.extend(scrape_novidades(content))
+
+    while len(url) < amount:
+        next_page_link = scrape_next_page_link(content)
+        next_page = fetch(next_page_link)
+        url.extend(scrape_novidades(next_page))
+
+    for i in url[:amount]:
+        news = fetch(i)
+        news_links.append(scrape_noticia(news))
+
+    create_news(news_links)
+    return news_links
