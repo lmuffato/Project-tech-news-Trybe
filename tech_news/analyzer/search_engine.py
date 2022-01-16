a8402e5b78f8acc6b4459ec2271228f5d81e3f5f
Renzo Sevilha <sevilharenzo@gmail.com>
2022-01-16 18:17:52 -0300

    feat: search_engine

--- tech_news/analyzer/search_engine.py
+++ tech_news/analyzer/search_engine.py
@@ -1,18 +1,62 @@
+from datetime import datetime
+from tech_news.database import db
+
+
 # Requisito 6
 def search_by_title(title):
-    """Seu código deve vir aqui"""
+    news = list(
+        db.news.find(
+            {"title": {"$regex": title, "$options": "i"}},
+            {"_id": False, "title": True, "url": True},
+        )
+    )
+    return parse_news(news)
 
 
 # Requisito 7
 def search_by_date(date):
-    """Seu código deve vir aqui"""
+    validate_date(date)
+
+    news = list(
+        db.news.find(
+            {"timestamp": {"$regex": date}},
+            {"_id": False, "title": True, "url": True},
+        )
+    )
+
+    return parse_news(news)
 
 
 # Requisito 8
 def search_by_source(source):
-    """Seu código deve vir aqui"""
+    news = list(
+        db.news.find(
+            {"sources": {"$regex": source, "$options": "i"}},
+            {"_id": False, "title": True, "url": True},
+        )
+    )
+
+    return parse_news(news)
 
 
 # Requisito 9
 def search_by_category(category):
-    """Seu código deve vir aqui"""
+    news = list(
+        db.news.find(
+            {"categories": {"$regex": category, "$options": "i"}},
+            {"_id": False, "title": True, "url": True},
+        )
+    )
+
+    return parse_news(news)
+
+
+def parse_news(news):
+    return [(new["title"], new["url"]) for new in news]
+
+
+def validate_date(date):
+    try:
+        datetime.strptime(date, "%Y-%m-%d")
+    except ValueError:
+        raise ValueError("Data inválida")
