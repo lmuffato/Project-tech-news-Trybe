e8e5cebc8ec64671a434dc9b2a7cf3343e38c48e
NilsonRCS <nilson.ribeiro.souza3@gmail.com>
2022-01-16 15:14:41 -0300

    iniciando o projeto tech-news

--- tech_news/scraper.py
+++ tech_news/scraper.py
@@ -1,6 +1,18 @@
+import requests
+import time
+
+
 # Requisito 1
 def fetch(url):
-    """Seu código deve vir aqui"""
+    time.sleep(1)
+    try:
+        res = requests.get(url, timeout=3)
+        if res.status_code == 200:
+            return res.text
+        else:
+            return None
+    except res.ReadTimeout:
+        return None
 
 
 # Requisito 2
