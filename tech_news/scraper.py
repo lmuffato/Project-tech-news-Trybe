77733b361b3fd9a5eceb46009047467031394a6c
Luciano Amâncio <madruga665@hotmail.com>
2022-01-17 11:45:30 -0300

    requisito 2

--- tech_news/scraper.py
+++ tech_news/scraper.py
@@ -1,5 +1,6 @@
-import time
 import requests
+import time
+from parsel import Selector
 
 
 # Requisito 1
@@ -7,7 +8,7 @@ def fetch(url):
     try:
         time.sleep(1)
         response = requests.get(url, timeout=3)
-        if (response.status_code != 200):
+        if response.status_code != 200:
             return None
         return response.text
     except requests.Timeout:
@@ -16,7 +17,11 @@ def fetch(url):
 
 # Requisito 2
 def scrape_novidades(html_content):
-    """Seu código deve vir aqui"""
+    data = Selector(text=html_content)
+    links = data.css(
+        "h3.tec--card__title a.tec--card__title__link::attr(href)"
+    ).getall()
+    return links
 
 
 # Requisito 3
@@ -32,3 +37,8 @@ def scrape_noticia(html_content):
 # Requisito 5
 def get_tech_news(amount):
     """Seu código deve vir aqui"""
+
+
+test = fetch("https://www.tecmundo.com.br/novidades")
+print(scrape_novidades(test))
+print(len(scrape_novidades(test)))
---
+++ tech_news/tempCodeRunnerFile.py
@@ -0,0 +1,1 @@
+from parsel import Selector
