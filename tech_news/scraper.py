d5bbfcc4c30bf84a562293c0407a4086fa5733d2
Carlos Sá <carlos25xrm@gmail.com>
2022-01-17 13:21:12 -0300

    feat: requisito 5

--- tech_news/scraper.py
+++ tech_news/scraper.py
@@ -1,9 +1,10 @@
 import time
 import requests
 import parsel
+from .database import create_news
 
 
-# Requisito 1
+# Requisito 1 - Pega o conteúdo do site
 def fetch(url):
     try:
         # README: "utilizar time.sleep(1) antes de cada requisição"
@@ -19,7 +20,7 @@ def fetch(url):
         return None
 
 
-# Requisito 2
+# Requisito 2 - Separa os links de notícias
 def scrape_novidades(html_content):
     conteudo = parsel.Selector(html_content)
     links = []
@@ -31,7 +32,7 @@ def scrape_novidades(html_content):
     return links
 
 
-# Requisito 3
+# Requisito 3 - Verifica se tem botão para próxima página
 def scrape_next_page_link(html_content):
     conteudo = parsel.Selector(html_content)
     botao = conteudo.css("a.tec--btn::attr(href)").get()
@@ -42,9 +43,10 @@ def scrape_next_page_link(html_content):
         return None
 
 
-# Requisito 4
+# Requisito 4 - Pega os detalhes da notícia
 # Gabriel Pereira, Eduardo Costa, Tiago Santos e EU estudamos
 # juntos o requisito 4 na Room 2 do plantão do dia 12/01/2022
+
 
 def qual_seletor(conteudo):
     # Para resolver o erro do avaliador local nesta função "qual_seletor",
@@ -100,12 +102,13 @@ def scrape_noticia(html_content):
     autor = qual_seletor(conteudo)
 
     # Regex aprendido com a instrução durante o plantão
-    envios = conteudo.css("div.tec--toolbar__item::text").re_first(r'\d+')
+    envios = conteudo.css("div.tec--toolbar__item::text").re_first(r"\d+")
     compartilhamentos = int(envios) if envios else 0
 
     comentarios = int(conteudo.css("button.tec--btn::attr(data-count)").get())
     sumario = "".join(
-        conteudo.css("div.tec--article__body p:first-child *::text").getall())
+        conteudo.css("div.tec--article__body p:first-child *::text").getall()
+    )
     fontes = fontes_da_noticia(conteudo)
     categorias = lista_de_categorias(conteudo)
 
@@ -123,6 +126,29 @@ def scrape_noticia(html_content):
 # print(scrape_noticia(a))
 
 
-# Requisito 5
+# Requisito 5 - Separa a quantidade de notícias desejadas
 def get_tech_news(amount):
-    """Seu código deve vir aqui"""
+    # Usa o site do Tecmundo como referência
+    URL_noticias = fetch("https://www.tecmundo.com.br/novidades")
+
+    # Separa os links das noticias da 1ª página
+    links_noticias = scrape_novidades(URL_noticias)
+
+    # Verifica se a quantidade de notícias desejadas pode ser encontrada
+    # apenas na 1ª página. Se a quantidade for insuficiente, vai pegar
+    # notícias das próximas páginas e juntar com as notícias da 1ª página.
+    while len(links_noticias) < amount:
+        proxima_pagina = scrape_next_page_link(URL_noticias)
+        noticias_proxima_pagina = fetch(proxima_pagina)
+        links_noticias.extend(scrape_novidades(noticias_proxima_pagina))
+
+    noticias = []
+
+    for link in links_noticias[:amount]:
+        novo_conteudo = fetch(link)
+        nova_noticia = scrape_noticia(novo_conteudo)
+        noticias.append(nova_noticia)
+
+    create_news(noticias)
+
+    return noticias
