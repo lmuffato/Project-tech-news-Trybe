18cd3312253dfaba3baf739360270353f58021cb
PollyanaOliveira <pollyana.oliveir@gmail.com>
2022-01-17 10:39:28 -0400

    feat: :sparkles: Requirement 4

--- tech_news/scraper.py
+++ tech_news/scraper.py
@@ -1,6 +1,9 @@
 from time import sleep
 import requests
 from parsel import Selector
+from .scraper_news import get_url, get_title, get_timestamp, get_summary
+from .scraper_news import get_writer, get_shares_count, get_comments_count
+from .scraper_news import get_sources, get_categories
 
 
 URL_BASE = 'https://www.tecmundo.com.br/novidades'
@@ -39,7 +42,21 @@ def scrape_next_page_link(html_content):
 
 # Requisito 4
 def scrape_noticia(html_content):
-    """Seu código deve vir aqui"""
+    selector = Selector(text=html_content)
+
+    news = {
+        'url': get_url(selector),
+        'title': get_title(selector),
+        'timestamp': get_timestamp(selector),
+        'writer': get_writer(selector),
+        'shares_count': get_shares_count(selector),
+        'comments_count': get_comments_count(selector),
+        'summary': get_summary(selector),
+        'sources': get_sources(selector),
+        'categories': get_categories(selector),
+    }
+
+    return news
 
 
 # Requisito 5
