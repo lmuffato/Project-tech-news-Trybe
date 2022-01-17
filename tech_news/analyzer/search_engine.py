fdfbaf7f287808e8d812046ecb69c6ce4ee24843
Eduardo Costa Maciel <costaeduardomaciel@gmail.com>
2022-01-17 10:07:42 -0300

    Feat: Implementa funçao 'search_by_general' e Fix: Refatora as funçoes 'search_by_title', 'search_by_date', 'search_by_source' e 'search_by_category'

--- tech_news/analyzer/search_engine.py
+++ tech_news/analyzer/search_engine.py
@@ -2,51 +2,37 @@ from ..database import search_news
 from time import strptime
 
 
+def search_by_general(str_colum, str_criteria):
+    data_news_list = []
+    list_news_data = search_news(
+        {str_colum: {"$regex": str_criteria, "$options": "i"}}
+    )
+    for list in list_news_data:
+        data_news_list.append((list["title"], list["url"]))
+
+    return data_news_list
+
+
 # Requisito 6
 def search_by_title(title):
-    list_news = search_news({"title": {"$regex": title, "$options": "i"}})
-    data_news_list = []
-
-    for list in list_news:
-        data_news_list.append((list["title"], list["url"]))
-    return data_news_list
+    return search_by_general("title", title)
 
 
 # Requisito 7
 def search_by_date(date):
-    data_news_list = []
     try:
         strptime(date, "%Y-%m-%d")
-        list_news_data = search_news(
-            {"timestamp": {"$regex": date, "$options": "i"}}
-        )
-        for list in list_news_data:
-            data_news_list.append((list["title"], list["url"]))
     except ValueError:
         raise ValueError("Data inválida")
 
-    return data_news_list
+    return search_by_general("timestamp", date)
 
 
 # Requisito 8
 def search_by_source(source):
-    data_news_list = []
-    list_news_data = search_news(
-        {"sources": {"$regex": source, "$options": "i"}}
-    )
-    for list in list_news_data:
-        data_news_list.append((list["title"], list["url"]))
-
-    return data_news_list
+    return search_by_general("sources", source)
 
 
 # Requisito 9
 def search_by_category(category):
-    data_news_list = []
-    list_news_data = search_news(
-        {"categories": {"$regex": category, "$options": "i"}}
-    )
-    for list in list_news_data:
-        data_news_list.append((list["title"], list["url"]))
-
-    return data_news_list
+    return search_by_general("categories", category)
