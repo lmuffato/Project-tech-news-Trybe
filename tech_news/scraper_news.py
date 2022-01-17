63719884f0ee1d5ca3393c64636f2f3e934e60f9
PollyanaOliveira <pollyana.oliveir@gmail.com>
2022-01-17 10:36:23 -0400

    feat: :sparkles: create def get_writer helper for requirement 4

--- tech_news/scraper_news.py
+++ tech_news/scraper_news.py
@@ -18,3 +18,25 @@ def get_summary(selector):
     summaries = 'div.tec--article__body > p:nth-child(1) *::text'
     summary = selector.css(summaries).getall()
     return ''.join(summary)
+
+
+def get_writer(selector):
+
+    writers = [
+        '.tec--timestamp:nth-child(1) a::text',
+        '.tec--author__info p:first-child::text',
+        '.tec--author__info p:first-child a::text',
+    ]
+
+    selector_writers = []
+    for author_selector in writers:
+        selected_writers = selector.css(author_selector).get()
+        if selected_writers is not None:
+            selector_writers.append(selected_writers.strip())
+        if selected_writers is None:
+            selector_writers.append(None)
+
+    author = [writer for writer in selector_writers if writer]
+    if len(author) == 0:
+        return None
+    return author[0]
