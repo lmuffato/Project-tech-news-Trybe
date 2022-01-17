8d0ccea14e88400bb17e2c2239c69fb09db649e3
Eduardo Seije <eduardoseije@gmail.com>
2022-01-17 20:28:48 -0300

    Feat Req 01 - criação da função fetch para fazer a requisição da url

--- tech_news/scraper.py
+++ tech_news/scraper.py
@@ -1,6 +1,19 @@
+from time import sleep
+import requests
+
+
 # Requisito 1
 def fetch(url):
-    """Seu código deve vir aqui"""
+    # """Seu código deve vir aqui"""
+    sleep(1)
+    try:
+        html = requests.get(url, timeout=3)
+    except requests.Timeout:
+        return None
+    if html.status_code == 200:
+        return html.text
+    else:
+        return None
 
 
 # Requisito 2
