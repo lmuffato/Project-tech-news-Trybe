fa73d9bdcfe3163463cd483f9374fe953e0f4c13
Eduardo Seije <eduardoseije@gmail.com>
2022-01-17 20:42:07 -0300

    Feat Req 02 - criação da função scrape_novidades

--- tech_news/scraper.py
+++ tech_news/scraper.py
@@ -1,13 +1,17 @@
 from time import sleep
 import requests
+import parsel
+URL = 'https://www.tecmundo.com.br/novidades'
 
 
 # Requisito 1
 def fetch(url):
     # """Seu código deve vir aqui"""
     sleep(1)
+    # Garante o intervalo de 1 segundo entre cada requisição.
     try:
         html = requests.get(url, timeout=3)
+        # Permite http requests usando python
     except requests.Timeout:
         return None
     if html.status_code == 200:
@@ -18,7 +22,11 @@ def fetch(url):
 
 # Requisito 2
 def scrape_novidades(html_content):
-    """Seu código deve vir aqui"""
+    # """Seu código deve vir aqui"""
+    selector = parsel.Selector(html_content)
+    return selector.css(
+        'main .tec--card__title__link::attr(href)'
+    ).getall()
 
 
 # Requisito 3
