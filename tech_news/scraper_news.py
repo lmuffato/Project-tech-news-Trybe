6341395f24545b4f500941f9c9f4a077c91db7a6
PollyanaOliveira <pollyana.oliveir@gmail.com>
2022-01-17 10:34:44 -0400

    feat: :sparkles: create def get_title helper for requirement 4

--- tech_news/scraper_news.py
+++ tech_news/scraper_news.py
@@ -1,3 +1,8 @@
 def get_url(selector):
     url = selector.css('head link[rel=canonical]::attr(href)').get()
     return url
+
+
+def get_title(selector):
+    title = selector.css('.tec--article__header__title::text').get()
+    return title
