ab632ea1537ac11cd65e5bda16e8e10e09493a1c
Gabriel L Ferreira <78182689+gabriellferreira-dev@users.noreply.github.com>
2022-01-17 18:30:49 -0300

    feat: requiriment 1

--- tech_news/scraper.py
+++ tech_news/scraper.py
@@ -1,6 +1,18 @@
+import time
+import requests
+
+
 # Requisito 1
 def fetch(url):
-    """Seu código deve vir aqui"""
+    time.sleep(1)
+
+    try:
+        response = requests.get(url, timeout=3)
+        if response.status_code == 200:
+            return response.text
+        return None
+    except requests.ReadTimeout:
+        return None
 
 
 # Requisito 2
