5e8042e6724b9555e1c6f56954bcf72003686470
Lucas André <lucas.cancado89@gmail.com>
2022-01-17 20:58:59 -0300

    req11

--- tech_news/analyzer/ratings.py
+++ tech_news/analyzer/ratings.py
@@ -1,6 +1,12 @@
+from tech_news.database import find_news
+
+
 # Requisito 10
 def top_5_news():
-    """Seu código deve vir aqui"""
+    all_ns = find_news()
+    sn = sorted(all_ns, key=lambda k: -k["comments_count"] - k["shares_count"])
+    top_5 = sn[0:5]
+    return [(news["title"], news["url"]) for news in top_5]
 
 
 # Requisito 11
