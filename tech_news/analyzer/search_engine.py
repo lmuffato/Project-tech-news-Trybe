06a87c62b862a029d77e5e60de5fe9298799ec3d
Lucas Machado <lucas.almeida.machado92@gmail.com>
2022-01-18 01:07:20 -0300

    Requisito 7: busca news atraves da hora

--- tech_news/analyzer/search_engine.py
+++ tech_news/analyzer/search_engine.py
@@ -1,4 +1,5 @@
 from tech_news.database import search_news
+import datetime
 
 
 # https://kb.objectrocket.com/mongo-db/how-to-query-mongodb-documents-with-regex-in-python-362
@@ -14,9 +15,26 @@ def search_by_title(title):
     return new_tupla
 
 
+# https://qastack.com.br/programming/16870663/how-do-i-validate-a-date-string-format-in-python
 # Requisito 7
 def search_by_date(date):
     """Seu código deve vir aqui"""
+    try:
+        datetime.datetime.strptime(date, "%Y-%m-%d")
+    except ValueError:
+        raise ValueError("Data inválida")
+
+    find_bd_news = search_news({"timestamp": {"$regex": date}})
+
+    new_tupla = []
+    for new in find_bd_news:
+        if new is not []:
+            new_tupla.append((new["title"], new["url"]))
+        else:
+            return []
+
+    return new_tupla
+
 
 
 # Requisito 8
