011bf6667fd35de46fc484e03b3ff4501e72a579
Debora <deborinhapassos@gmail.com>
2022-01-18 17:44:31 -0300

    Req 4

--- tech_news/scraper.py
+++ tech_news/scraper.py
@@ -42,8 +42,62 @@ def scrape_next_page_link(html_content):
 
 
 # Requisito 4
+def categories_list(html_code):
+    categories = []
+    search_categories = html_code.css("div#js-categories a::text").getall()
+    for search in search_categories:
+        categories.append(search.strip())
+    return categories
+
+
+def writer_selector(html_code):
+    way01 = html_code.css(".tec--timestamp__item a::text").get()
+    way02 = html_code.css("a.tec--author__info__link::text").get()
+    way03 = html_code.css(".tec--author__info > p:first-child::text").get()
+    if way01 is not None:
+        writer = way01.strip()
+        return writer
+    elif way02 is not None:
+        writer = way02.strip()
+        return writer
+    elif way03 is not None:
+        writer = way03.strip()
+        return writer
+    else:
+        return None
+
+
+def info_source(html_code):
+    list_source = []
+    search_source = html_code.css("div.z--mb-16 a::text").getall()
+
+    for source in search_source:
+        list_source.append(source.strip())
+
+    return list_source
+
+
 def scrape_noticia(html_content):
-    """Seu código deve vir aqui"""
+    html_code = parsel.Selector(html_content)
+    info = {}
+    url = html_code.css("head link[rel=canonical]::attr(href)").get()
+    title = html_code.css("h1.tec--article__header__title::text").get()
+    timestamp = html_code.css("time#js-article-date::attr(datetime)").get()
+    writer = writer_selector(html_code)
+    shares = html_code.css("div.tec--toolbar__item::text").re_first(r"\d+")
+    users_shares = int(shares) if shares else 0
+    comments = int(html_code.css("button.tec--btn::attr(data-count)").get())
+    summary = "".join(
+        html_code.css(
+            "div.tec--article__body > p:first-child *::text").getall()
+    )
+    sources = info_source(html_code)
+    categories = categories_list(html_code)
+
+    info.update(url=url, title=title, timestamp=timestamp, writer=writer)
+    info.update(shares_count=users_shares, comments_count=comments)
+    info.update(summary=summary, sources=sources, categories=categories)
+    return info
 
 
 # Requisito 5
