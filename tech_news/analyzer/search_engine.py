d63c009c5ec37f5e10ffd125777b83f750f57237
jonathan <jonathanfernandesjf98@gmail.com>
2022-01-18 18:56:17 -0300

    requisito 07

--- tech_news/analyzer/search_engine.py
+++ tech_news/analyzer/search_engine.py
@@ -1,4 +1,5 @@
 from tech_news.database import search_news
+import re
 
 
 # Requisito 6
@@ -16,6 +17,21 @@ def search_by_title(title):
 # Requisito 7
 def search_by_date(date):
     """Seu código deve vir aqui"""
+    print("print da data no inicio", date, type(date))
+    news = []
+    validate = re.search(r"\d{4}-\d{2}-\d{2}", date)
+    data_dia = date.split("-")[-1]
+    data_mes = date.split("-")[1]
+    valida_data_nao_existente = True
+    if (data_mes == "02" and int(data_dia) > 29) or int(data_mes) > 12:
+        valida_data_nao_existente = None
+    if validate is None or valida_data_nao_existente is None:
+        raise ValueError("Data inválida")
+    db_news = search_news(
+        {"timestamp": {"$regex": date, "$options": "i"}})
+    for notice in db_news:
+        news.append((notice["title"], notice["url"]))
+    return news
 
 
 # Requisito 8
