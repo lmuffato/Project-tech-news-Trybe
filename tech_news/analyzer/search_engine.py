2b2dcde50f48d85f503bfe951ba7d67fbf1f90d2
samuelmmjr <samuelmmjunior@gmail.com>
2022-01-18 09:51:57 -0300

    feat: req7

--- tech_news/analyzer/search_engine.py
+++ tech_news/analyzer/search_engine.py
@@ -1,4 +1,5 @@
 from tech_news.database import search_news
+import time
 
 
 # Requisito 6
@@ -14,7 +15,17 @@ def search_by_title(title):
 
 # Requisito 7
 def search_by_date(date):
-    """Seu código deve vir aqui"""
+    try:
+        time.strptime(date, "%Y-%m-%d")
+        data = search_news({"timestamp": {"$regex": date}})
+        noticias = []
+        for noticia in data:
+            nova_noticia = (noticia["title"], noticia["url"])
+            noticias.append(nova_noticia)
+        return noticias
+
+    except ValueError:
+        raise ValueError("Data inválida")
 
 
 # Requisito 8
