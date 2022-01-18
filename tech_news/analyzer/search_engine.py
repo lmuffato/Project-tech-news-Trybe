36af0c033e0861ea94502e22343ee3993ce8128c
Mathaus Annibal <mathausannibal@gmail.com>
2022-01-17 23:07:53 -0300

    adiciona requisito 6

--- tech_news/analyzer/search_engine.py
+++ tech_news/analyzer/search_engine.py
@@ -1,6 +1,16 @@
+from tech_news.database import search_news
+
+
 # Requisito 6
 def search_by_title(title):
     """Seu código deve vir aqui"""
+    data_base = search_news({"title": {"$regex": title, "$options": "i"}})
+    noticias = []
+
+    for noticia in data_base:
+        news = (noticia["title"], noticia["url"])
+        noticias.append(news)
+    return noticias
 
 
 # Requisito 7
