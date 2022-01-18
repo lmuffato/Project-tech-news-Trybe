4f63bbea4ceae9b7f60d324ce3cf467aec91e77f
Nathalia Zebral <natzebral@gmail.com>
2022-01-18 12:06:03 -0300

    requisito 6

--- tech_news/analyzer/search_engine.py
+++ tech_news/analyzer/search_engine.py
@@ -1,6 +1,15 @@
+from tech_news.database import search_news
+
+
 # Requisito 6
 def search_by_title(title):
-    """Seu código deve vir aqui"""
+    search = search_news({"title": {"$regex": title, "$options": "i"}})
+    news_list = []
+    for news in search:
+        selected_news = (news["title"], news["url"])
+        news_list.append(selected_news)
+
+    return news_list
 
 
 # Requisito 7
