edef9210b1b368a0c49b1282badb02f5c66dd4ef
Robertson <robertson.nunes@gmail.com>
2022-01-18 16:53:13 -0300

    feat req7. implements search by date

--- tech_news/analyzer/search_engine.py
+++ tech_news/analyzer/search_engine.py
@@ -1,4 +1,5 @@
 from tech_news.database import search_news
+import datetime
 
 
 # Requisito 6
@@ -16,6 +17,17 @@ def search_by_title(title):
 # Requisito 7
 def search_by_date(date):
     """Seu código deve vir aqui"""
+    try:
+        # https://docs.python.org/3/library/datetime.html#strftime-strptime-behavior
+        datetime.datetime.strptime(date, "%Y-%m-%d")
+    except ValueError:
+        raise ValueError("Data inválida")
+    query = {"timestamp": {"$regex": f"{date}"}}
+    arrNews = search_news(query)
+    if arrNews:
+        return [(new["title"], new["url"]) for new in arrNews]
+    else:
+        return []
 
 
 # Requisito 8
