32edbfdd3a559c8d083702e715a0e8a1995684e0
Lucas Lara <lucasml@live.com>
2022-01-18 18:58:11 -0300

    Requisito 8

--- tech_news/analyzer/search_engine.py
+++ tech_news/analyzer/search_engine.py
@@ -31,9 +31,25 @@ def search_by_date(date):
 
 # Requisito 8
 def search_by_source(source):
-    """Seu código deve vir aqui"""
+    find_by_source = search_news(
+        {"sources": {"$regex": source, "$options": "i"}})
+
+    find_result = []
+    for news in find_by_source:
+        found_note = (news["title"], news["url"])
+        find_result.append(found_note)
+
+    return find_result
 
 
 # Requisito 9
 def search_by_category(category):
-    """Seu código deve vir aqui"""
+    find_by_category = search_news(
+        {"categories": {"$regex": category, "$options": "i"}})
+
+    find_result = []
+    for news in find_by_category:
+        found_note = (news["title"], news["url"])
+        find_result.append(found_note)
+
+    return find_result
