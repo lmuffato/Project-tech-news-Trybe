ed40a680652a1ae4044594019b6a4a6144d9239f
Tiago Santos <tiago881@gmail.com>
2022-01-18 12:09:40 -0300

    feat: ✨ requirement 6

--- tech_news/analyzer/search_engine.py
+++ tech_news/analyzer/search_engine.py
@@ -1,6 +1,17 @@
-# Requisito 6
+from tech_news.database import search_news
+
+
 def search_by_title(title):
-    """Seu código deve vir aqui"""
+    search_for_news = search_news(
+        {"title": {"$regex": title, "$options": "i"}}
+    )
+    list_news = []
+
+    for result in search_for_news:
+        news_result = (result["title"], result["url"])
+        list_news.append(news_result)
+
+    return list_news
 
 
 # Requisito 7
