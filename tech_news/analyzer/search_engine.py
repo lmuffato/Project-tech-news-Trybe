0b02503c9eae34d319ac717734bc68c81612d906
Lucas Lara <lucasml@live.com>
2022-01-18 17:47:13 -0300

    Requisito 7

--- tech_news/analyzer/search_engine.py
+++ tech_news/analyzer/search_engine.py
@@ -1,4 +1,5 @@
 from tech_news.database import search_news
+from time import strptime
 
 
 # Requisito 6
@@ -14,7 +15,18 @@ def search_by_title(title):
 
 # Requisito 7
 def search_by_date(date):
-    """Seu código deve vir aqui"""
+    try:
+        strptime(date, "%Y-%m-%d")
+        find_by_date = search_news({"timestamp": {"$regex": date}})
+        find_result = []
+        for news in find_by_date:
+            found_note = (news["title"], news["url"])
+            find_result.append(found_note)
+
+        return find_result
+
+    except ValueError:
+        raise ValueError("Data inválida")
 
 
 # Requisito 8
