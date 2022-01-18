247fbdcb8ae97a92592b7a5a81cae00c9d75f887
guidugaich <guidugaich.dev@gmail.com>
2022-01-18 17:42:17 -0300

    req 5 - work in progress

--- tech_news/scraper.py
+++ tech_news/scraper.py
@@ -38,8 +38,54 @@ def scrape_next_page_link(html_content):
 
 
 # Requisito 4
+def get_categories_noticia(html_content):
+    selector = Selector(text=html_content)
+
+    categories_query = '.tec--badge--primary::text'
+
+    return selector.css(categories_query).getall()
+
+
+def get_sources_noticia(html_content):
+    selector = Selector(text=html_content)
+
+    categories = get_categories_noticia(html_content)
+
+    sources_and_categs_query = '.tec--badge::text'
+    sources_and_categories = selector.css(sources_and_categs_query).getall()
+
+    return [item for item in sources_and_categories if item not in categories]
+
+
+def get_summary_noticia(html_content):
+    selector = Selector(text=html_content)
+
+    summary_query = '.tec--article__body p:first-child *::text'
+
+    return ''.join(selector.css(summary_query).getall())
+
+
 def scrape_noticia(html_content):
-    """Seu código deve vir aqui"""
+    selector = Selector(text=html_content)
+
+    url_query = 'head link[rel=canonical]::attr(href)'
+    title_query = '.tec--article__header__title::text'
+    timestamp_query = '#js-article-date::attr(datetime)'
+    writer_query = '.tec--author__info__link::text'
+    shares_count_query = '.tec--toolbar__item::text'
+    comments_count_query = '#js-comments-btn::attr(data-count)'
+
+    return {
+        'url': selector.css(url_query).get(),
+        'title': selector.css(title_query).get(),
+        'timestamp': selector.css(timestamp_query).get(),
+        'writer': selector.css(writer_query).get(),
+        'shares_count': selector.css(shares_count_query).get(),
+        'comments_count': selector.css(comments_count_query).get(),
+        'summary': get_summary_noticia(html_content),
+        'sources': get_sources_noticia(html_content),
+        'categories': get_categories_noticia(html_content)
+    }
 
 
 # Requisito 5
