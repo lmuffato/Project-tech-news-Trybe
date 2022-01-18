7abf2d87e63d0f830fe51802f601b8ea77c35efc
Lucas Machado <lucas.almeida.machado92@gmail.com>
2022-01-17 22:26:54 -0300

    Requisito 6 - buscando pelo titulo - retornando tuplas

--- tech_news/analyzer/search_engine.py
+++ tech_news/analyzer/search_engine.py
@@ -1,6 +1,17 @@
+from tech_news.database import search_news
+
+
+# https://kb.objectrocket.com/mongo-db/how-to-query-mongodb-documents-with-regex-in-python-362
 # Requisito 6
 def search_by_title(title):
     """Seu código deve vir aqui"""
+    find_bd_news = search_news({"title": {"$regex": title,
+                                          "$options": 'i'}})
+
+    new_tupla = []
+    for new in find_bd_news:
+        new_tupla.append((new["title"], new["url"]))
+    return new_tupla
 
 
 # Requisito 7
