e3bc2c2e2bf35ae29275fd6a81a37166818e6ba2
guidugaich <guidugaich.dev@gmail.com>
2022-01-18 20:14:44 -0300

    req 5 - in progress using class

--- tech_news/scraper.py
+++ tech_news/scraper.py
@@ -38,53 +38,84 @@ def scrape_next_page_link(html_content):
 
 
 # Requisito 4
-def get_categories_noticia(html_content):
-    selector = Selector(text=html_content)
+class Noticia:
+    def __init__(self, html_content):
+        self.html_content = html_content
+        self.selector = Selector(text=self.html_content)
 
-    categories_query = '.tec--badge--primary::text'
+    def get_categories(self):
+        categories_query = '.tec--badge--primary::text'
 
-    return selector.css(categories_query).getall()
+        categories = self.selector.css(categories_query).getall()
+        return [cat.strip() for cat in categories]
 
+    def get_sources(self):
+        categories = self.get_categories()
 
-def get_sources_noticia(html_content):
-    selector = Selector(text=html_content)
+        sources_and_categs_query = '.tec--badge::text'
+        sources_and_categs = self.selector.css(
+            sources_and_categs_query).getall()
 
-    categories = get_categories_noticia(html_content)
+        sources_and_categs = [sc.strip() for sc in sources_and_categs]
 
-    sources_and_categs_query = '.tec--badge::text'
-    sources_and_categories = selector.css(sources_and_categs_query).getall()
+        return [tag for tag in sources_and_categs if tag not in categories]
 
-    return [item for item in sources_and_categories if item not in categories]
+    def get_summary(self):
+        summary_query = '.tec--article__body p:first-child *::text'
 
+        return ''.join(self.selector.css(summary_query).getall())
 
-def get_summary_noticia(html_content):
-    selector = Selector(text=html_content)
+    def get_comments_count(self):
+        comments_count_query = '#js-comments-btn::attr(data-count)'
 
-    summary_query = '.tec--article__body p:first-child *::text'
+        return int(self.selector.css(comments_count_query).get())
 
-    return ''.join(selector.css(summary_query).getall())
+    def get_shares_count(self):
+        shares_count_query = '.tec--toolbar__item::text'
+
+        shares_string = self.selector.css(shares_count_query).get()
+        shares_count = [
+            int(c) for c in shares_string.split() if c.isdigit()
+        ][0]
+
+        return shares_count
+
+    def get_writer(self):
+        writer_query = '.tec--author__info__link::text'
+
+        writer = self.selector.css(writer_query).get()
+        print(writer)
+        return writer.strip()
+
+    def get_timestamp(self):
+        timestamp_query = '#js-article-date::attr(datetime)'
+
+        return self.selector.css(timestamp_query).get()
+
+    def get_title(self):
+        title_query = '.tec--article__header__title::text'
+
+        return self.selector.css(title_query).get()
+
+    def get_url(self):
+        url_query = 'head link[rel=canonical]::attr(href)'
+
+        return self.selector.css(url_query).get()
 
 
 def scrape_noticia(html_content):
-    selector = Selector(text=html_content)
-
-    url_query = 'head link[rel=canonical]::attr(href)'
-    title_query = '.tec--article__header__title::text'
-    timestamp_query = '#js-article-date::attr(datetime)'
-    writer_query = '.tec--author__info__link::text'
-    shares_count_query = '.tec--toolbar__item::text'
-    comments_count_query = '#js-comments-btn::attr(data-count)'
+    noticia = Noticia(html_content)
 
     return {
-        'url': selector.css(url_query).get(),
-        'title': selector.css(title_query).get(),
-        'timestamp': selector.css(timestamp_query).get(),
-        'writer': selector.css(writer_query).get(),
-        'shares_count': selector.css(shares_count_query).get(),
-        'comments_count': selector.css(comments_count_query).get(),
-        'summary': get_summary_noticia(html_content),
-        'sources': get_sources_noticia(html_content),
-        'categories': get_categories_noticia(html_content)
+        'url': noticia.get_url(),
+        'title': noticia.get_title(),
+        'timestamp': noticia.get_timestamp(),
+        'writer': noticia.get_writer(),
+        'shares_count': noticia.get_shares_count(),
+        'comments_count': noticia.get_comments_count(),
+        'summary': noticia.get_summary(),
+        'sources': noticia.get_sources(),
+        'categories': noticia.get_categories()
     }
 
 
