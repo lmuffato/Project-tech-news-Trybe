a25f1d4d43400a53741e5d8cc6aa9995fdc7fa8f
Rodrigo Rech <rodrigorech92@gmail.com>
2022-01-18 09:51:30 -0300

    Feat: search_by_date

--- tech_news/analyzer/search_engine.py
+++ tech_news/analyzer/search_engine.py
@@ -1,3 +1,4 @@
+import datetime
 from tech_news.database import search_news
 
 
@@ -15,7 +16,22 @@ def search_by_title(title):
 
 # Requisito 7
 def search_by_date(date):
-    """Seu código deve vir aqui"""
+    # https://www.kite.com/python/answers/how-to-validate-a-date-string-format-in-python
+    format = "%Y-%m-d"
+
+    try:
+        datetime.datetime.strptime(date, format)
+    except ValueError:
+        raise ValueError("Data inválida")
+
+    db_news = search_news({"timestamp": {"$regex": date, "$options": "i"}})
+    list_of_news = []
+
+    for news in db_news:
+        news_to_be_added = (news["title"], news["url"])
+        list_of_news.append(news_to_be_added)
+
+    return list_of_news
 
 
 # Requisito 8
