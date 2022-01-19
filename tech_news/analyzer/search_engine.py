717fbe1de1786956311c96d70bed6da6aa109f42
rafael <rafittu@outlook.com>
2022-01-19 19:07:34 -0300

    feat req 6 - search news by title function

--- tech_news/analyzer/search_engine.py
+++ tech_news/analyzer/search_engine.py
@@ -1,6 +1,15 @@
+from tech_news.database import search_news
+
+
 # Requisito 6
 def search_by_title(title):
-    """Seu código deve vir aqui"""
+    news = search_news({"title": {"$regex": title, "$options": "i"}})
+    tech_news_tupla = []
+
+    for news in news:
+        tech_news_tupla.append((news["title"], news["url"]))
+
+    return tech_news_tupla
 
 
 # Requisito 7
