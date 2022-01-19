9631d3a9bedaf6ff297b508e4b5738caac2bd53c
Patrick Morais <ppternunes@gmail.com>
2022-01-19 13:08:11 -0300

    fix flake

--- tech_news/analyzer/ratings.py
+++ tech_news/analyzer/ratings.py
@@ -1,17 +1,16 @@
 from tech_news.database import find_news
 
 
-
 # Requisito 10
-
 def popularity(new):
     return new["shares_count"] + new["comments_count"]
+
 
 def top_5_news():
     news_list = find_news()
     if not news_list:
         return []
-        
+
     news_list.sort(reverse=True, key=popularity)
 
     return [(new["title"], new["url"]) for new in news_list[:5]]
