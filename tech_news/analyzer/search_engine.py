4296853b376486611be2cc85024861204e8ab570
igson <igson660@gmail.com>
2022-01-18 21:33:26 -0500

    quesito 06

--- tech_news/analyzer/search_engine.py
+++ tech_news/analyzer/search_engine.py
@@ -1,6 +1,16 @@
+from tech_news.database import search_news
+
+
 # Requisito 6
 def search_by_title(title):
-    """Seu código deve vir aqui"""
+    query = {"title": {"$regex": title, "$options": "i"}}
+    news = search_news(query)
+    data = []
+
+    for news in news:
+        data.append((news["title"], news["url"]))
+
+    return data
 
 
 # Requisito 7
