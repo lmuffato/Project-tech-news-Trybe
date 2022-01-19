f38c8bb8d8551f5ba3b5c2235ee5de7b2046714f
negres <nataliaknegres@gmail.com>
2022-01-19 10:03:13 -0300

    add req 4

--- tech_news/scraper.py
+++ tech_news/scraper.py
@@ -41,23 +41,72 @@ def scrape_next_page_link(html_content):
 
 # Requisito 4
 def scrape_noticia(html_content):
-    """Seu código deve vir aqui"""
+    selector = Selector(html_content)
+    url = selector.css("head link[rel=canonical]::attr(href)").get()
+    title = selector.css("#js-article-title::text").get()
+    timestamp = selector.css(
+        ".tec--timestamp__item time::attr(datetime)"
+    ).get()
+
+    writer = selector.css(".z--font-bold *::text").get()
+    if writer:
+        writer = writer.strip()
+
+    shares = selector.css(".tec--toolbar__item::text").get()
+    if shares:
+        shares = int(shares.split()[0])
+    else:
+        shares = 0
+
+    comments = selector.css("#js-comments-btn::attr(data-count)").get()
+
+    summary = "".join(
+        selector.css(".tec--article__body > p:nth-child(1) *::text").getall()
+    )
+
+    categories = [
+        category.strip()
+        for category in selector.css("#js-categories a::text").getall()
+    ]
+
+    sources = [
+        source.strip()
+        for source in selector.css(
+            ".z--mb-16 .tec--badge::text"
+        ).getall()
+    ]
+
+    data = {
+        "url": url,
+        "title": title,
+        "timestamp": timestamp,
+        "writer": writer,
+        "shares_count": shares,
+        "comments_count": int(comments),
+        "summary": summary,
+        "categories": categories,
+        "sources": sources,
+    }
+
+    return data
 
 
 # Requisito 5
 def get_tech_news(amount):
     html_content = fetch("https://www.tecmundo.com.br/novidades")
-    arr = scrape_novidades(html_content)
-    while len(arr) < amount:
+    urls = []
+    urls.extend(scrape_novidades(html_content))
+
+    while len(urls) < amount:
         next = scrape_next_page_link(html_content)
         page = fetch(next)
-        arr.extend(scrape_novidades(page))
+        urls.extend(scrape_novidades(page))
 
     news = []
-    for item in arr[:amount]:
-        new_page = fetch(item)
-        new_news = scrape_noticia(new_page)
-        news.append(new_news)
+
+    for url in urls[:amount]:
+        html_content = fetch(url)
+        news.append(scrape_noticia(html_content))
 
     create_news(news)
     return news
