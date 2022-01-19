56b131f2b9ba883904cbeb09950a18b86650bb0b
Eduardo Seije <eduardoseije@gmail.com>
2022-01-18 22:47:18 -0300

    Feat Req 10 - Criação da função popularity e top 5 news

--- tech_news/analyzer/ratings.py
+++ tech_news/analyzer/ratings.py
@@ -1,6 +1,18 @@
+from tech_news.database import find_news
+
+
+def popularity(new):
+    return new["shares_count"] + new["comments_count"]
+
+
 # Requisito 10
 def top_5_news():
-    """Seu código deve vir aqui"""
+    # """Seu código deve vir aqui"""
+    news = find_news()
+    if not news:
+        return []
+    news.sort(reverse=True, key=popularity)
+    return [(new["title"], new["url"]) for new in news[:5]]
 
 
 # Requisito 11
