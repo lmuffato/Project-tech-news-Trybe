9e2758f740959f338de027354704f664b897c724
Luciano Amâncio <madruga665@hotmail.com>
2022-01-18 21:47:45 -0300

    requisito 6

--- tech_news/analyzer/search_engine.py
+++ tech_news/analyzer/search_engine.py
@@ -1,6 +1,16 @@
+from tech_news.database import search_news
+
+
 # Requisito 6
 def search_by_title(title):
-    """Seu código deve vir aqui"""
+    find_news = search_news({"title": {"$regex": title, "$options": "i"}})
+    list_news = []
+
+    for news in find_news:
+        result = (news["title"], news["url"])
+        list_news.append(result)
+
+    return list_news
 
 
 # Requisito 7
@@ -16,3 +26,6 @@ def search_by_source(source):
 # Requisito 9
 def search_by_category(category):
     """Seu código deve vir aqui"""
+
+
+print(search_by_title("Sherlock"))
