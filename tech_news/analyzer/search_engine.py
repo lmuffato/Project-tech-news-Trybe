3bd78a134a277a253a00a80f61ce1c694c2bf6b8
Rmgreco <rafaelmgreco@hotmail.com>
2022-01-19 08:54:08 -0300

    req 2-9

--- tech_news/analyzer/search_engine.py
+++ tech_news/analyzer/search_engine.py
@@ -1,18 +1,51 @@
+from datetime import datetime
+from re import IGNORECASE, compile
+
+from tech_news.database import search_news
+
+# https://stackoverflow.com/questions/500864/case-insensitive-regular-expression-without-re-compile
+
+
 # Requisito 6
 def search_by_title(title):
     """Seu código deve vir aqui"""
+    news = search_news({"title": compile(title, IGNORECASE)})
+    results = list()
+    for new in news:
+        results.append((new["title"], new["url"]))
+    return results
 
 
 # Requisito 7
 def search_by_date(date):
     """Seu código deve vir aqui"""
+    try:
+        datetime.strptime(date, "%Y-%m-%d")
+    except ValueError:
+        raise ValueError("Data inválida")
+
+    news = search_news({"timestamp": compile(date)})
+    result = list()
+    for new in news:
+        result.append((new["title"], new["url"]))
+    return result
 
 
 # Requisito 8
 def search_by_source(source):
     """Seu código deve vir aqui"""
+    news = search_news({"sources": compile(source, IGNORECASE)})
+    result = list()
+    for new in news:
+        result.append((new["title"], new["url"]))
+    return result
 
 
 # Requisito 9
 def search_by_category(category):
     """Seu código deve vir aqui"""
+    news = search_news({"categories": compile(category, IGNORECASE)})
+    result = list()
+    for new in news:
+        result.append((new["title"], new["url"]))
+    return result
--- tech_news/scraper.py
+++ tech_news/scraper.py
@@ -1,6 +1,9 @@
 from time import sleep
 
 import requests
+from parsel import Selector
+
+from tech_news.database import create_news
 
 
 # Requisito 1
@@ -19,18 +22,71 @@ def fetch(url):
 # Requisito 2
 def scrape_novidades(html_content):
     """Seu código deve vir aqui"""
+    site_content = Selector(html_content)
+    links_news = []
+    for link in site_content.css("h3.tec--card__title"):
+        new_link = link.css("a.tec--card__title__link::attr(href)").get()
+        links_news.append(new_link)
+    return links_news
 
 
 # Requisito 3
 def scrape_next_page_link(html_content):
     """Seu código deve vir aqui"""
+    selector = Selector(text=html_content)
+    return selector.css(".tec--list > a::attr(href)").get()
 
 
 # Requisito 4
 def scrape_noticia(html_content):
     """Seu código deve vir aqui"""
+    selector = Selector(text=html_content)
+    result = dict()
+    result["url"] = selector.css("link[rel=canonical]::attr(href)").get()
+    result["title"] = selector.css(".tec--article__header__title::text").get()
+    result["timestamp"] = selector.css(
+        ".tec--timestamp__item time::attr(datetime)"
+    ).get()
+    writer = selector.css(".z--font-bold *::text").get().strip()
+    result["writer"] = writer
+    share_counts = selector.css(".tec--toolbar__item::text").get()
+    result["shares_count"] = (
+        int(share_counts.strip().split()[0]) if share_counts else 0
+    )
+    result["comments_count"] = int(
+        selector.css("#js-comments-btn::attr(data-count)").get()
+    )
+    result["summary"] = "".join(
+        selector.css(".tec--article__body > p:nth-child(1) ::text").getall()
+    )
+    result["sources"] = [
+        source.strip()
+        for source in selector.css(".z--mb-16 div .tec--badge::text").getall()
+    ]
+    result["categories"] = [
+        category.strip()
+        for category in selector.css(
+            "#js-categories .tec--badge::text"
+        ).getall()
+    ]
+    return result
 
 
 # Requisito 5
 def get_tech_news(amount):
     """Seu código deve vir aqui"""
+    link = fetch("https://www.tecmundo.com.br/novidades")
+    result = list()
+
+    while len(result) < amount:
+        for url in scrape_novidades(link):
+            if len(result) < amount:
+                news = fetch(url)
+                result.append(scrape_noticia(news))
+
+        if len(result) < amount:
+            next_page = scrape_next_page_link(link)
+            link = fetch(next_page)
+
+    create_news(result)
+    return result
