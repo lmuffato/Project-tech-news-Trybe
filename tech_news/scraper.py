d60f9b268d9eb3a0aadd553a47d8e0f9c8b57b00
vinicios <soicinivt@gmail.com>
2022-01-19 16:36:46 -0300

    feat(req 04): function scrape_noticia in scraper.py

--- tech_news/scraper.py
+++ tech_news/scraper.py
@@ -42,7 +42,59 @@ def scrape_next_page_link(html_content):
 
 # Requisito 4
 def scrape_noticia(html_content):
-    """Seu código deve vir aqui"""  # Nem papagaio repete tanto a mesma frase
+    data = Selector(text=html_content)
+
+    url = data.css("head link[rel=canonical]::attr(href)").get()
+
+    title = data.css("#js-article-title::text").get()
+
+    timestamp = data.css(".tec--timestamp__item time::attr(datetime)").get()
+
+    writer = (
+        data.css(".tec--timestamp__item.z--font-bold a::text").get()
+        or data.css(".z--m-none.z--truncate.z--font-bold a::text").get()
+        or data.css(
+            ".tec--author__info.z--min-w-none.z--flex.z--flex-col p::text"
+        ).get()
+    )
+
+    if writer is not None:
+        writer = writer.strip()
+
+    shares_count = data.css(".tec--toolbar__item::text").get() or 0
+
+    if shares_count != 0:
+        shares_count = int(shares_count.split()[0])
+
+    comments_count = int(data.css("#js-comments-btn::attr(data-count)").get())
+
+    summary = "".join(
+        data.css(
+            ".tec--article__body.p402_premium p:first-child *::text"
+        ).getall()
+    )
+
+    sources = [
+        source.strip()
+        for source in data.css(".z--mb-16 h2 + div .tec--badge::text").getall()
+    ]
+
+    categories = [
+        categorie.strip()
+        for categorie in data.css("#js-categories a::text").getall()
+    ]
+
+    return {
+        "url": url,
+        "title": title,
+        "timestamp": timestamp,
+        "writer": writer,
+        "shares_count": shares_count,
+        "comments_count": comments_count,
+        "summary": summary,
+        "sources": sources,
+        "categories": categories,
+    }
 
 
 # Requisito 5
