07fcf8a3acb8320d42264f125221e4cf9814344c
VRodriguera <V.rodriguesjk@gmail.com>
2022-01-18 21:14:44 -0300

    feat: :sparkles: Req.7

--- tech_news/analyzer/search_engine.py
+++ tech_news/analyzer/search_engine.py
@@ -1,4 +1,5 @@
 from tech_news.database import search_news
+import time
 
 
 # REQUISITO 6
@@ -12,9 +13,20 @@ def search_by_title(title):
     return fetch_result
 
 
-# Requisito 7
+# REQUISITO 7
 def search_by_date(date):
-    """Seu código deve vir aqui"""
+    try:
+        time.strptime(date, "%Y-%m-%d")
+        fetch_by_date = search_news({"timestamp": {"$regex": date}})
+        fetch_result = []
+        for news in fetch_by_date:
+            found_note = (news["title"], news["url"])
+            fetch_result.append(found_note)
+
+        return fetch_result
+
+    except ValueError:
+        raise ValueError("Data inválida")
 
 
 # Requisito 8
