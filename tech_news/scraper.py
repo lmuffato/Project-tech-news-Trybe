14c54b1b9312a1939c01990bff63d68626c83189
Anderson F. S. do Nascimento <anderson.nascimentoafsn@gmail.com>
2022-01-19 07:22:32 -0300

    requeriment 3

--- tech_news/scraper.py
+++ tech_news/scraper.py
@@ -30,7 +30,12 @@ def scrape_novidades(html_content):
 
 # Requisito 3
 def scrape_next_page_link(html_content):
-    """Seu código deve vir aqui"""
+    selector = parsel.Selector(html_content)
+    nextLinkPage = selector.css("a.tec--btn::attr(href)").get()
+
+    if nextLinkPage:
+        return nextLinkPage
+    return None
 
 
 # Requisito 4
