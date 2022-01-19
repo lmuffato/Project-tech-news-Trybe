2f9f9206a77fac60aa8097064a26a28776c1f854
Patrick Morais <ppternunes@gmail.com>
2022-01-19 13:07:22 -0300

    req 10

--- tech_news/analyzer/ratings.py
+++ tech_news/analyzer/ratings.py
@@ -1,6 +1,20 @@
+from tech_news.database import find_news
+
+
+
 # Requisito 10
+
+def popularity(new):
+    return new["shares_count"] + new["comments_count"]
+
 def top_5_news():
-    """Seu código deve vir aqui"""
+    news_list = find_news()
+    if not news_list:
+        return []
+        
+    news_list.sort(reverse=True, key=popularity)
+
+    return [(new["title"], new["url"]) for new in news_list[:5]]
 
 
 # Requisito 11
