38362d6077f04160e00b3442f4fcb231fbdb68b1
NilsonRCS <nilson.ribeiro.souza3@gmail.com>
2022-01-19 11:41:48 -0300

    feat:requirement:09, test

--- tech_news/analyzer/search_engine.py
+++ tech_news/analyzer/search_engine.py
@@ -39,4 +39,10 @@ def search_by_source(source):
 
 # Requisito 9
 def search_by_category(category):
-    """Seu código deve vir aqui"""
+    find_category = search_news(
+        {"categories": {"$regex": category, "$options": "i"}})
+    result = []
+    for news in find_category:
+        note = (news["title"], news["url"])
+        result.append(note)
+    return result
