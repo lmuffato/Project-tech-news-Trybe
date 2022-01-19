532508cc45b2656cbbb8bb43356a2e67b820ed8b
rafael <rafittu@outlook.com>
2022-01-19 20:20:46 -0300

    feat req 10 - get top five news function

--- tech_news/analyzer/ratings.py
+++ tech_news/analyzer/ratings.py
@@ -1,6 +1,25 @@
+from tech_news.database import find_news
+
+
 # Requisito 10
 def top_5_news():
-    """Seu código deve vir aqui"""
+    get_news = find_news()
+    top_five = []
+
+    most_popular_news = sorted(
+        get_news,
+        key=lambda news: news["shares_count"] +
+        news["comments_count"], reverse=True
+    )
+
+    for news in most_popular_news:
+        top_five.append((news["title"], news["url"]))
+
+    return top_five[:5]
+
+    # Referências para fazer requisito 10:
+    # https://github.com/tryber/sd-010-a-tech-news/blob/rodolfo-oliveira-tech-news/tech_news/analyzer/ratings.py
+    # https://stackoverflow.com/questions/8966538/syntax-behind-sortedkey-lambda
 
 
 # Requisito 11
