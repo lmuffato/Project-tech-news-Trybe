325da22c38cdc19f70bdec4595979d729cfb2575
Micaela Alves <micaela.alves93@gmail.com>
2022-01-19 11:47:39 -0300

    feat - req 5 - 5/6

--- tech_news/scraper.py
+++ tech_news/scraper.py
@@ -1,9 +1,10 @@
 import requests
 import time
 from parsel import Selector
+from tech_news.database import create_news
 
 
-# Requisito 1
+# Requisito 1 - obtem conteudo HTML
 def fetch(url):
     """Seu código deve vir aqui"""
     time.sleep(1)
@@ -17,7 +18,7 @@ def fetch(url):
         return None
 
 
-# Requisito 2
+# Requisito 2 - lista de links
 def scrape_novidades(html_content):
     """Seu código deve vir aqui"""
     selector = Selector(text=html_content)
@@ -28,7 +29,7 @@ def scrape_novidades(html_content):
     return list_href
 
 
-# Requisito 3
+# Requisito 3 - link da proxima pagina
 def scrape_next_page_link(html_content):
     """Seu código deve vir aqui"""
     selector = Selector(text=html_content)
@@ -36,7 +37,7 @@ def scrape_next_page_link(html_content):
     return list_href
 
 
-# Requisito 4
+# Requisito 4 - conteudo da noticia
 def scrape_noticia(html_content):
     """Seu código deve vir aqui"""
     selector = Selector(text=html_content)
@@ -53,14 +54,16 @@ def scrape_noticia(html_content):
         shares_count = int(shares_count.strip()[0])
     else:
         shares_count = 0
-    comments_count = selector.css(
-        ".tec--toolbar .tec--toolbar__item::text"
-    ).get()
-    if comments_count is not None:
-        comments_count = int(comments_count.strip()[0])
-    else:
-        comments_count = 0
-    summary = selector.css("head meta[name*=description]::attr(content)").get()
+    comments_count = selector.css("#js-comments-btn::attr(data-count)").get()
+    # if comments_count is not None:
+    #     comments_count = int(comments_count.strip()[0])
+    # else:
+    #     comments_count = 0
+    summary = "".join(
+        selector.css(".tec--article__body > p:nth-child(1) *::text").getall()
+    )
+    # summary = selector.css("head
+    # meta[name*=description]::attr(content)").get()
     sources = selector.css(".z--mb-16 .tec--badge::text").getall()
     categories = selector.css(".tec--badge--primary *::text").getall()
 
@@ -70,13 +73,30 @@ def scrape_noticia(html_content):
         "timestamp": timestamp,
         "writer": writer,
         "shares_count": shares_count,
-        "comments_count": comments_count,
+        "comments_count": int(comments_count),
         "summary": summary,
         "sources": [source.strip() for source in sources],
         "categories": [cat.strip() for cat in categories],
     }
 
 
-# Requisito 5
+# Requisito 5 - salvar o conteudo de n noticias no mongodb
 def get_tech_news(amount):
     """Seu código deve vir aqui"""
+    url = "https://www.tecmundo.com.br/novidades"
+    page = fetch(url)
+    list_url = scrape_novidades(page)
+
+    while len(list_url) <= amount:
+        next_url = scrape_next_page_link(page)
+        next_page = fetch(next_url)
+        list_url.extend(scrape_novidades(next_page))
+
+    tech_news = [
+        scrape_noticia(fetch(new))
+        for new in list_url
+        if list_url.index(new) <= amount - 1
+    ]
+    create_news(tech_news)
+    print(len(tech_news))
+    return tech_news
