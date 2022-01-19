98fe4ad50eef6159a926b62161b77308df72db5e
Anderson F. S. do Nascimento <anderson.nascimento@meta.com.br>
2022-01-19 08:06:40 -0300

    requeriment 6

--- tech_news/analyzer/search_engine.py
+++ tech_news/analyzer/search_engine.py
@@ -1,6 +1,14 @@
+import re
+from tech_news.database import search_news
+
+
 # Requisito 6
 def search_by_title(title):
-    """Seu código deve vir aqui"""
+    news = [
+        (noticia["title"], noticia["url"])
+        for noticia in search_news({"title": re.compile(title, re.IGNORECASE)})
+    ]
+    return news
 
 
 # Requisito 7
