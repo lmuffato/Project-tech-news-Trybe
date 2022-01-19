7ea1a60ee8602c9b91f5d5a13830097a8a4a0bfb
Patrick Morais <ppternunes@gmail.com>
2022-01-19 12:54:00 -0300

    req 7 - fix flake

--- tech_news/analyzer/search_engine.py
+++ tech_news/analyzer/search_engine.py
@@ -1,11 +1,32 @@
+from tech_news.database import search_news
+import time
+
+
 # Requisito 6
 def search_by_title(title):
-    """Seu código deve vir aqui"""
+    news = search_news({"title": {"$regex": title, "$options": "i"}})
+    results_list = []
+    for new in news:
+        tupla = (new["title"], new["url"])
+        results_list.append(tupla)
+    return results_list
 
 
 # Requisito 7
 def search_by_date(date):
-    """Seu código deve vir aqui"""
+    # ref
+    # https://stackoverflow.com/questions/16527878/check-if-line-is-a-timestamp-in-python
+    try:
+        time.strptime(date, "%Y-%m-%d")
+    except ValueError:
+        raise ValueError("Data inválida")
+
+    news = search_news({"timestamp": {"$regex": date}})
+    results_list = []
+    for new in news:
+        tupla = (new["title"], new["url"])
+        results_list.append(tupla)
+    return
 
 
 # Requisito 8
--- tech_news/scraper.py
+++ tech_news/scraper.py
@@ -1,6 +1,8 @@
 import requests
 import time
+
 from parsel import Selector
+from tech_news.database import create_news
 
 
 # Requisito 1
@@ -37,10 +39,102 @@ def scrape_next_page_link(html_content):
 
 
 # Requisito 4
+# Feito com ajuda
+# e referência de https://github.com/tryber/sd-010-a-tech-news/pull/39
+def get_url(selector):
+    current_url = selector.css("head link[rel=canonical]::attr(href)").get()
+    return current_url
+
+
+def get_title(selector):
+    title = selector.css("h1#js-article-title::text").get()
+    return title
+
+
+def get_date(selector):
+    date = selector.css("time#js-article-date::attr(datetime)").get()
+    return date
+
+
+def get_author(selector):
+    name = selector.css("a.tec--author__info__link::text").get()
+    if name:
+        return name.strip()
+    otherName = selector.css("div.tec--timestamp__item a::text").get()
+    if otherName:
+        return otherName.strip()
+    oneMore = selector.css(
+        "div.tec--author__info > p:first_child *::text").get()
+    if oneMore:
+        return oneMore.strip()
+    return None
+
+
+def get_share(selector):
+    shares = selector.css("div.tec--toolbar__item::text").get()
+    return int(shares.split()[0]) if shares else 0
+
+
+def get_comments(selector):
+    comments = selector.css("button#js-comments-btn::attr(data-count)").get()
+    return int(comments)
+
+
+def get_first_paragraph(selector):
+    paragraph = selector.css(
+        ".tec--article__body > p:first_child *::text").getall()
+    return "".join(paragraph)
+
+
+def get_sources(selector):
+    sources = selector.css("div.z--mb-16 a::text").getall()
+    all_sources = []
+    for source in sources:
+        all_sources.append(source.strip())
+    return all_sources
+
+
+def get_categories(selector):
+    categories = selector.css("#js-categories a::text").getall()
+    all_categories = []
+    for categorie in categories:
+        all_categories.append(categorie.strip())
+    return all_categories
+
+
 def scrape_noticia(html_content):
-    """Seu código deve vir aqui"""
+    selector = Selector(html_content)
+    return {
+        "url": get_url(selector),
+        "title": get_title(selector),
+        "timestamp": get_date(selector),
+        "writer": get_author(selector),
+        "shares_count": get_share(selector),
+        "comments_count": get_comments(selector),
+        "summary": get_first_paragraph(selector),
+        "sources": get_sources(selector),
+        "categories": get_categories(selector)
+    }
 
 
 # Requisito 5
 def get_tech_news(amount):
-    """Seu código deve vir aqui"""
+    url = "https://www.tecmundo.com.br/novidades"
+    content = fetch(url)
+    news_urls = scrape_novidades(content)
+    news_list = []
+
+    while len(news_urls) < amount:
+        url = scrape_next_page_link(content)
+        content = fetch(url)
+        add_url = scrape_novidades(content)
+        news_urls.extend(add_url)
+
+    for new_url in news_urls:
+        if len(news_list) < amount:
+            new_content = fetch(new_url)
+            scraped = scrape_noticia(new_content)
+            news_list.append(scraped)
+
+    create_news(news_list)
+    return news_list
