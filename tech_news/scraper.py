072327288749fa284b9acb7205d349d284dd9625
vinicios <soicinivt@gmail.com>
2022-01-19 10:38:19 -0300

    start project

--- tech_news/scraper.py
+++ tech_news/scraper.py
@@ -1,23 +1,23 @@
 # Requisito 1
 def fetch(url):
-    """Seu código deve vir aqui"""
+    """Seu código deve vir aqui"""  # Entendi
 
 
 # Requisito 2
 def scrape_novidades(html_content):
-    """Seu código deve vir aqui"""
+    """Seu código deve vir aqui"""  # Pode deixar
 
 
 # Requisito 3
 def scrape_next_page_link(html_content):
-    """Seu código deve vir aqui"""
+    """Seu código deve vir aqui"""  # Ok
 
 
 # Requisito 4
 def scrape_noticia(html_content):
-    """Seu código deve vir aqui"""
+    """Seu código deve vir aqui"""  # Nem papagaio repete tanto a mesma frase
 
 
 # Requisito 5
 def get_tech_news(amount):
-    """Seu código deve vir aqui"""
+    """Seu código deve vir aqui"""  # Meu código vai aonde eu quiser parceiro
