995bd82014340a0092bd79e8c7d177e430eae675
William <w.willers@outlook.com>
2022-01-19 11:33:07 -0400

    Ultimos reqs

--- tech_news/analyzer/search_engine.py
+++ tech_news/analyzer/search_engine.py
@@ -1,18 +1,35 @@
+from tech_news.database import search_news
+from datetime import datetime
+import re
+
+
+# https://blog.geekhunter.com.br/python-regex/
+# https://docs.python.org/3/library/re.html
+
 # Requisito 6
 def search_by_title(title):
-    """Seu código deve vir aqui"""
+    news = search_news({"title": re.compile(title, re.IGNORECASE)})
+    return [(item["title"], item["url"]) for item in news]
 
 
 # Requisito 7
 def search_by_date(date):
-    """Seu código deve vir aqui"""
+    # https://www.programiz.com/python-programming/datetime/strptime
+    try:
+        datetime.strptime(date, "%Y-%m-%d")
+    except ValueError:
+        raise ValueError("Data inválida")
+    news = search_news({"timestamp": {"$regex": date}})
+    return [(item["title"], item["url"]) for item in news]
 
 
 # Requisito 8
 def search_by_source(source):
-    """Seu código deve vir aqui"""
+    news = search_news({"sources": re.compile(source, re.IGNORECASE)})
+    return [(item["title"], item["url"]) for item in news]
 
 
 # Requisito 9
 def search_by_category(category):
-    """Seu código deve vir aqui"""
+    news = search_news({"categories": re.compile(category, re.IGNORECASE)})
+    return [(item["title"], item["url"]) for item in news]
