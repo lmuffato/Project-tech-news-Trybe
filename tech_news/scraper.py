9b2c6f511fe8bc6923fb4734c2c1da3a17a9894d
Patrick Morais <ppternunes@gmail.com>
2022-01-19 11:36:25 -0300

    reqs 1 2 3

--- tech_news/scraper.py
+++ tech_news/scraper.py
@@ -1,16 +1,39 @@
+import requests
+import time
+from parsel import Selector
+
+
 # Requisito 1
 def fetch(url):
-    """Seu código deve vir aqui"""
+    time.sleep(1)
+    try:
+        response = requests.get(url, timeout=3)
+    except requests.Timeout:
+        return None
+
+    if response.status_code != 200:
+        return None
+    return response.text
 
 
 # Requisito 2
 def scrape_novidades(html_content):
-    """Seu código deve vir aqui"""
+    selector = Selector(html_content)
+    news_list = []
+    for news in selector.css("h3.tec--card__title a::attr(href)").getall():
+        news_list.append(news)
+
+    return news_list
 
 
 # Requisito 3
 def scrape_next_page_link(html_content):
-    """Seu código deve vir aqui"""
+    selector = Selector(html_content)
+    next_page_link = selector.css("a.tec--btn::attr(href)").get()
+
+    if next_page_link:
+        return next_page_link
+    return None
 
 
 # Requisito 4
