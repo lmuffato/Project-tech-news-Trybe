28038dffe21d76c8bedf49cb6d8a48c5c3b140fd
Nicholas Torres J. Vasconcelos <nicolastorresjatoba@gmail.com>
2022-01-19 11:53:41 -0300

    Requisitos 1 a 9 concluidos

--- tech_news/analyzer/search_engine.py
+++ tech_news/analyzer/search_engine.py
@@ -1,18 +1,53 @@
-# Requisito 6
+from tech_news.database import find_news
+from datetime import datetime
+
+
 def search_by_title(title):
-    """Seu código deve vir aqui"""
+    content = find_news()
+    result = list()
+    for new in content:
+        if new["title"].upper() == title.upper():
+            result.append(tuple([new["title"], new["url"]]))
+    return result or list()
 
 
-# Requisito 7
 def search_by_date(date):
-    """Seu código deve vir aqui"""
+    date_formating = "%Y-%m-%d"
+    try:
+        datetime.strptime(date, date_formating)
+        content = find_news()
+        response = list()
+        for new in content:
+            if new["timestamp"][0:10] == date:
+                response.append(tuple([new["title"], new["url"]]))
+        return response or []
+    except ValueError:
+        raise ValueError("Data inválida")
 
 
-# Requisito 8
 def search_by_source(source):
-    """Seu código deve vir aqui"""
+    content = find_news()
+    response = list()
+
+    for new in content:
+        sources = list()
+
+        for source_item in new["sources"]:
+            sources.append(source_item.upper())
+
+        if source.upper() in sources:
+            response.append(tuple([new["title"], new["url"]]))
+
+    return response or []
 
 
-# Requisito 9
 def search_by_category(category):
-    """Seu código deve vir aqui"""
+    content = find_news()
+    response = list()
+    for new in content:
+        categories = list()
+        for categories_item in new["categories"]:
+            categories.append(categories_item.upper())
+        if category.upper() in categories:
+            response.append(tuple([new["title"], new["url"]]))
+    return response or []
--- tech_news/scraper.py
+++ tech_news/scraper.py
@@ -1,23 +1,72 @@
-# Requisito 1
+import time
+import requests
+from tech_news.database import create_news
+from parsel import Selector
+
+
 def fetch(url):
-    """Seu código deve vir aqui"""
+    try:
+        content = requests.get(url, timeout=3)
+        time.sleep(1)
+        if content.status_code == 200:
+            return content.text
+        return None
+    except requests.ReadTimeout:
+        return None
 
 
-# Requisito 2
 def scrape_novidades(html_content):
-    """Seu código deve vir aqui"""
+    selector = Selector(html_content)
+    i = selector.css("h3 > a[class='tec--card__title__link']::attr(href)")
+    return i.getall()
 
 
-# Requisito 3
 def scrape_next_page_link(html_content):
-    """Seu código deve vir aqui"""
+    selector = Selector(html_content).css(".tec--btn::attr(href)").get()
+    return selector
 
 
-# Requisito 4
 def scrape_noticia(html_content):
-    """Seu código deve vir aqui"""
+    selector = Selector(text=html_content)
+    news = dict()
+    news["url"] = selector.css("link[rel=canonical]::attr(href)").get()
+
+    news["title"] = selector.css("#js-article-title::text").get()
+
+    news["timestamp"] = selector.css("#js-article-date::attr(datetime)").get()
+
+    news["writer"] = selector.css(
+        '.z--font-bold *::text'
+        ).get().strip() if True else None
+
+    get_share = selector.css(".tec--toolbar__item::text").re_first(r"\d+")
+    news["shares_count"] = int(get_share) if get_share else 0
+
+    get_comments = selector.css("#js-comments-btn::text").re_first(r"\d+")
+    news["comments_count"] = int(get_comments) if get_comments else 0
+
+    news["summary"] = "".join(selector.css(
+        ".tec--article__body > p:first-of-type *::text"
+    ).getall()).strip()
+
+    get_source = selector.css(".z--mb-16 div a::text").getall()
+    news["sources"] = [source.strip() for source in get_source]
+
+    get_category = selector.css("#js-categories a::text").getall()
+    news["categories"] = [category.strip() for category in get_category]
+
+    return news
 
 
-# Requisito 5
 def get_tech_news(amount):
-    """Seu código deve vir aqui"""
+    url = "https://www.tecmundo.com.br/novidades"
+    titles = list()
+    news = list()
+    while len(news) < amount:
+        news = news + scrape_novidades(fetch(url))
+        url = scrape_next_page_link(fetch(url))
+    for content in news[:amount]:
+        content_link = fetch(content)
+        titles.append(scrape_noticia(content_link))
+    create_news(titles)
+    return titles
