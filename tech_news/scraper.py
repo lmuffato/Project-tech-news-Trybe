3e6da47eed4c4f3467e958b86125851461aa1e82
igson <igson660@gmail.com>
2022-01-18 21:18:08 -0500

    quesito 04

--- tech_news/scraper.py
+++ tech_news/scraper.py
@@ -1,5 +1,7 @@
 import time
+
 import requests
+
 from parsel import Selector
 
 
@@ -38,7 +40,44 @@ def scrape_next_page_link(html_content):
 
 # Requisito 4
 def scrape_noticia(html_content):
-    """Seu código deve vir aqui"""
+    selector = Selector(html_content)
+
+    return {
+        "url": selector.css("meta[property='og:url']::attr(content)").get(),
+        "title": selector.css("h1.tec--article__header__title::text").get(),
+        "timestamp": selector.css(
+          "div.tec--timestamp__item time::attr(datetime)"
+          ).get(),
+        "writer": (selector.css(
+          ".tec--author__info__link::text"
+          ).get() or selector.css(
+          "div.tec--timestamp__item a::text"
+          ).get() or selector.css(
+            "div.tec--author__info p.z--font-bold::text"
+            ).get() or None).strip() if (selector.css(
+              ".tec--author__info__link::text").get() or selector.css(
+                "div.tec--timestamp__item a::text").get() or selector.css(
+                  "div.tec--author__info p.z--font-bold::text"
+                ).get() or None) else None,
+        "shares_count": int((selector.css(
+          ".tec--toolbar__item::text"
+          ).get() or 0).split()[0]) if (selector.css(
+            ".tec--toolbar__item::text"
+            ).get() or 0) else 0,
+        "comments_count": int(selector.css(
+          "#js-comments-btn::attr(data-count)"
+          ).get()) if selector.css(
+            "#js-comments-btn::attr(data-count)").get() else 0,
+        "summary": ("".join(selector.css(
+          ".tec--article__body > p:first-child *::text"
+          ).getall())),
+        "sources": [item.strip() for item in selector.css(
+          "div.z--mb-16 div a::text"
+          ).getall()],
+        "categories": [item.strip() for item in selector.css(
+          "#js-categories a::text"
+          ).getall()],
+    }
 
 
 # Requisito 5
