c63139fee2ded539c81f913e138d72405cc2b4ab
rafael <rafittu@outlook.com>
2022-01-19 11:28:24 -0300

    feat req 3 - scrape next page function

--- tech_news/scraper.py
+++ tech_news/scraper.py
@@ -2,6 +2,7 @@ from parsel import Selector
 import time
 import requests
 
+
 # Requisito 1
 def fetch(url):
     time.sleep(1)
@@ -29,7 +30,10 @@ def scrape_novidades(html_content):
 
 # Requisito 3
 def scrape_next_page_link(html_content):
-    """Seu código deve vir aqui"""
+    selector = Selector(html_content)
+
+    next_page = selector.css("a.tec--btn::attr(href)").get()
+    return next_page
 
 
 # Requisito 4
