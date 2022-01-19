030983b747a26fea87f39660330ecdb23306f431
Breno <blf@ic.ufal.br>
2022-01-19 10:08:45 -0300

    feat: function scrape_noticia

--- tech_news/scraper.py
+++ tech_news/scraper.py
@@ -38,9 +38,54 @@ def scrape_next_page_link(html_content):
     return next_page_link
 
 
+# def get_author(html_content):
+#     """Função auxiliar que navega até o perfil do autor e busca o seu nome"""
+#     selector = parsel.Selector(html_content)
+#     url_author = selector.css
+
+
 # Requisito 4
 def scrape_noticia(html_content):
     """Seu código deve vir aqui"""
+    selector = parsel.Selector(html_content)
+    url = selector.css("[rel=canonical]::attr(href)").get()
+    title = selector.css("h1::text").get()
+    timestamp = selector.css("time::attr(datetime)").get()
+    writer = (
+        selector.css(".z--font-bold a::text").get()
+        or selector.css(".z--font-bold::text").get()
+    )
+    if writer is not None:
+        writer = writer.strip()
+
+    shares_count = selector.css(".tec--toolbar__item::text").get()
+    if shares_count is not None:
+        shares_count = shares_count.split()[0]
+    else:
+        shares_count = 0
+
+    comments_count = (
+        selector.css("#js-comments-btn::text").getall()[1].split()[0]
+    )
+    summary = "".join(
+        selector.css(".p402_premium p:first-child *::text").getall()
+    )
+    sources = selector.css(".z--mb-16 a *::text").getall()
+    sources = [source.strip() for source in sources]
+    categories = selector.css("#js-categories a *::text").getall()
+    categories = [category.strip() for category in categories]
+    return {
+        "url": url,
+        "title": title,
+        "timestamp": timestamp,
+        "writer": writer,
+        "shares_count": int(shares_count),
+        "comments_count": int(comments_count),
+        "summary": summary,
+        "sources": sources,
+        "categories": categories,
+    }
+    # return writer
 
 
 # Requisito 5
