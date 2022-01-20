d5af2c4219308a8bac7336cd3e5efa0fdd1df155
Edrei <edreimateus@gmail.com>
2022-01-20 00:46:56 -0300

    feat 1 requiriment

--- tech_news/scraper.py
+++ tech_news/scraper.py
@@ -1,6 +1,19 @@
+import time
+import requests
+
+
 # Requisito 1
 def fetch(url):
     """Seu código deve vir aqui"""
+    try:
+        time.sleep(1)
+        response = requests.get(url, timeout=3)
+    except requests.Timeout:
+        return None
+
+    if response.status_code != 200:
+        return None
+    return response.text
 
 
 # Requisito 2
