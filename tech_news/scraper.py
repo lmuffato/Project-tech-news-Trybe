5e840d88726a2ce3e53484ae29da415ffab6be9f
Felipe Shcolnik <shcolnik@gmail.com.br>
2022-01-20 16:51:04 -0300

    task4

--- tech_news/scraper.py
+++ tech_news/scraper.py
@@ -37,11 +37,116 @@ def scrape_next_page_link(html_content):
         return None
 
 
+def scrape_next_page_exemple(link):
+    response = requests.get(link)
+    selector = parsel.Selector(text=response.text)
+    next_page_url = selector.css(
+        ".tec--btn.tec--btn--lg.tec--btn--primary.z--mx-auto.z--mt-48::attr(href)"
+    ).get()
+    print(next_page_url)
+
+
+# scrape_next_page_exemple('https://www.tecmundo.com.br/novidades')
+
+
 # Requisito 4
+def find_writer(selector):
+    writer = (
+        selector.css(".tec--author__info__link ::text").get() or
+        selector.css(".tec--timestamp__item.z--font-bold ::text").get() or
+        selector.css(".z--m-none.z--truncate.z--font-bold ::text").get()
+    )
+    if writer:
+        writer = writer.strip()
+    return writer
+
+
+def shares_counter(selector):
+    shares_count_str = selector.css(".tec--toolbar__item ::text").get().strip()
+    if shares_count_str == '':
+        shares_count = 0
+    else:
+        shares_count = int(shares_count_str.split(" ")[0])
+    return shares_count
+
+
+def get_categories(selector):
+    categories_list = selector.css("#js-categories ::text").getall()
+    categories = []
+    for category in categories_list:
+        if category != ' ':
+            categories.append(category.strip())
+    return categories
+
+
 def scrape_noticia(html_content):
-    """Seu código deve vir aqui"""
+    selector = parsel.Selector(text=html_content)
+    url = selector.css("head link[rel=canonical] ::attr(href)").get()
+    title = selector.css("h1.tec--article__header__title::text").get()
+    timestamp = selector.css(
+        ".tec--timestamp__item time::attr(datetime)"
+        ).get()
+    comments_count = int(selector.css(
+        "#js-comments-btn ::attr(data-count)"
+        ).get())
+    if comments_count == '':
+        comments_count = 0
+    summary_list = selector.css(
+        ".tec--article__body p:first_child *::text"
+        ).getall()
+    summary = ''.join(summary_list)
+    sources_list = selector.css(".z--mb-16 .tec--badge ::text").getall()
+    sources = []
+    for source in sources_list:
+        if source != ' ':
+            sources.append(source.strip())
+    scrape_new = {
+        "url": url,
+        "title": title,
+        "timestamp": timestamp,
+        "writer": find_writer(selector),
+        "shares_count": shares_counter(selector),
+        "comments_count": comments_count,
+        "summary": summary,
+        "sources": sources,
+        "categories": get_categories(selector),
+    }
+    # print(scrape_new)
+    return scrape_new
+
+
+# print(scrape_noticia('https://www.tecmundo.com.br/dispositivos-moveis/215327-pixel-5a-tera-lancamento-limitado-devido-escassez-chips.htm'))
 
 
 # Requisito 5
 def get_tech_news(amount):
     """Seu código deve vir aqui"""
+
+
+# def noticia_html_v3():
+#     path = (
+#         "tests/"
+#         "assets/"
+#         "tecmundo_pages/"
+#         "seguranca|"
+#         "215274-pmes-principais-alvos-ataques-ciberneticos.htm."
+#         "html"
+#     )
+#     with open(path) as f:
+#         return f.read()
+
+
+# print(noticia_html_v3())
+
+# # Req.4
+# def test_scrape_noticia(noticia_html_v3):
+#     assert scrape_noticia(noticia_html_v3) == all_news[0]
+
+
+# def mocked_fetch(url):
+#     """Fake-fetches html from local file caches"""
+#     skip = len("https://www.tecmundo.com.br/")
+#     file_id = url[skip:].replace("/", "|")
+#     path = f"tests/assets/tecmundo_pages/{file_id}.html"
+#     with open(path) as cached_html:
+#         return cached_html.read()
