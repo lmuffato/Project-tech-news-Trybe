ce9db339405215e26d7fe3189be8e9bd6f8a328e
guidugaich <guidugaich.dev@gmail.com>
2022-01-20 12:15:47 -0300

    req 10 - top 5 news

--- tech_news/analyzer/ratings.py
+++ tech_news/analyzer/ratings.py
@@ -1,6 +1,15 @@
+from tech_news.database import find_news
+
+
 # Requisito 10
 def top_5_news():
-    """Seu código deve vir aqui"""
+    news = find_news()
+    for n in news:
+        n['rating'] = n['shares_count'] + n['comments_count']
+
+    sorted_news = sorted(news, key=lambda d: (-d['rating'], d['title']))
+
+    return [(n['title'], n['url']) for n in sorted_news][0:5]
 
 
 # Requisito 11
