d644a4084c82818c4a8eff64103d8eac5cf77d45
joaoAndradeJr <fullservicemanaus@gmail.com>
2022-01-20 20:11:57 -0400

    add: returns for req 4 ok, but not edited

--- tech_news/scraper.py
+++ tech_news/scraper.py
@@ -39,7 +39,19 @@ def scrape_next_page_link(html_content):
 
 # Requisito 4
 def scrape_noticia(html_content):
-    """Seu código deve vir aqui"""
+    selector = Selector(text=html_content)
+    url = selector.css("head > link[rel=canonical] ::attr(href)").get()
+    title = selector.css("#js-article-title ::text").get()
+    timestamp = selector.css("#js-article-date ::attr(datetime)").get()
+    author = selector.css(".tec--author__info__link ::text").get()
+    shares = selector.css(".tec--toolbar__item ::text").get()
+    comments = selector.css("#js-comments-btn ::attr(data-count)").get()
+    summary = selector.css(
+        ".tec--article__body > p:first_child *::text"
+    ).getall()
+    sources = selector.css(".tec--badge ::text").getall()
+    categories = selector.css("#js-categories ::text").getall()
+    print(categories)
 
 
 # Requisito 5
