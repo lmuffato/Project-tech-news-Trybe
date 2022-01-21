d56f685f7afd0d3afc9a8b0b0463e22052bbf5e9
Breno <blf@ic.ufal.br>
2022-01-21 00:20:08 -0300

    feat: function search_by_date

--- tech_news/analyzer/search_engine.py
+++ tech_news/analyzer/search_engine.py
@@ -1,17 +1,31 @@
-from tech_news.database import db
+from tech_news.database import search_news
+from datetime import datetime
 
 
 # Requisito 6
 def search_by_title(title):
     """Seu código deve vir aqui"""
     title = title.title()
-    results = db.news.find({"title": title})
+    query = {"title": title}
+    results = search_news(query)
     return [(result["title"], result["url"]) for result in results]
 
 
 # Requisito 7
 def search_by_date(date):
     """Seu código deve vir aqui"""
+    try:
+        """Source:
+        https://qastack.com.br/programming/
+        16870663/how-do-i-validate-a-date-string-format-in-python"""
+        datetime.strptime(date, "%Y-%m-%d")
+    except ValueError:
+        raise ValueError("Data inválida")
+
+    query = {"timestamp": {"$regex": date}}
+
+    results = search_news(query)
+    return [(result["title"], result["url"]) for result in results]
 
 
 # Requisito 8
