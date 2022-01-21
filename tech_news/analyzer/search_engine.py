abc7291142008d5afcc146160f420943c754478e
rcalvs <rafaelcalvette@gmail.com>
2022-01-21 14:13:09 -0300

    Req 06 OK

--- tech_news/analyzer/search_engine.py
+++ tech_news/analyzer/search_engine.py
@@ -1,6 +1,18 @@
+from ..database import search_news
+import datetime
+
+
 # Requisito 6
 def search_by_title(title):
     """Seu código deve vir aqui"""
+    db_data = search_news({"title": {"$regex": title, "$options": "i"}})
+    news_list = []
+
+    for item in db_data:
+        news = (item["title"], item["url"])
+        news_list.append(news)
+
+    return news_list
 
 
 # Requisito 7
