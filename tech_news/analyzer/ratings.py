071709b620709f0538e8525825330c48bb5295d2
rcalvs <rafaelcalvette@gmail.com>
2022-01-21 15:18:56 -0300

    Req 8-9-10

--- tech_news/analyzer/ratings.py
+++ tech_news/analyzer/ratings.py
@@ -1,7 +1,20 @@
+from tech_news.database import find_news
+
+
+def popularity(new):
+    return new["shares_count"] + new["comments_count"]
+
+
 # Requisito 10
 def top_5_news():
     """Seu código deve vir aqui"""
+    news_list = find_news()
+    if not news_list:
+        return []
 
+    news_list.sort(reverse=True, key=popularity)
+
+    return [(new["title"], new["url"]) for new in news_list[:5]]
 
 # Requisito 11
 def top_5_categories():
