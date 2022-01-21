594e2cc353310b7b8a365927283fd0583577f5da
luizfelipe-dev <thelastnailmusic@gmail.com>
2022-01-21 00:45:48 -0300

    4 in progress

--- tech_news/scraper.py
+++ tech_news/scraper.py
@@ -2,7 +2,6 @@
 import time
 import requests
 from parsel import Selector
-import util
 
 
 def fetch(url):
@@ -37,6 +36,74 @@ def scrape_next_page_link(html_content):
     return next_page_buttons
 
 
+######################################################################################
+# AUX
+
+def get_writer(selector):
+    writer1 = '.z--font-bold a ::text'
+    writer2 = '.z--font-bold ::text'
+
+    writer = selector.css(writer1).get()
+
+    if writer is None:
+        writer = selector.css(writer2).get()
+
+    return writer.strip()
+
+
+def get_shares_count(selector):
+    shares_count = selector.css(
+        "#js-author-bar > nav >"
+        "div:nth-child(1)::text"
+        ).get()
+
+    if shares_count is None or not ('Compartilharam'):
+        shares_count = 0
+
+    return shares_count
+
+
+def get_comments(selector):
+    comments_count = selector.css(
+        "#button#js-comments-btn"
+        "::attr(data-count)"
+        ).get()
+    if comments_count is None:
+        comments_count = 0
+
+    return int(comments_count)
+
+
+def get_summary(selector):
+    summary = "".join(
+        selector.css(
+            "div.tec--article__body > p:first-child *::text"
+        ).getall()
+    )
+    return summary
+
+
+def get_sources(selector):
+    arr = []
+    get_sources = selector.css("div.z--mb-16 a::text").getall()
+
+    for source in get_sources:
+        arr.append(source.strip())
+
+    return arr
+
+
+def get_categories(selector):
+    arr = []
+    categories = selector.css(
+            "#js-categories ::text"
+            ).getall()
+
+    for cat in categories:
+        arr.append(cat.strip())
+    return arr
+
+
 # Requisito 4
 def scrape_noticia(html_content):
     selector = Selector(html_content)
@@ -44,22 +111,18 @@ def scrape_noticia(html_content):
     url = selector.css("link[rel=canonical] ::attr(href)").get()
     title = selector.css("#js-article-title ::text").get()
     timestamp = selector.css("#js-article-date > strong ::text").get()
-    writer = selector.css(
-        "div.z--pt-40.z--pb-24 > div.z--flex.z--items-center"
-        "> div.tec--timestamp.tec--timestamp--lg >"
-        "div.tec--timestamp__item.z--font-bold > a *::text"
-        ).get()
+    writer = get_writer(selector)
 
     return {
         "url": url,
         "title": title,
         "timestamp": timestamp,
         "writer": writer,
-        "shares_count": util.get_shares_count(selector),
-        "comments_count": util.get_comments(selector),
-        "summary": util.get_summary(selector),
-        "sources": util.get_sources(selector),
-        "categories": util.get_categories(selector),
+        "shares_count": get_shares_count(selector),
+        "comments_count": get_comments(selector),
+        "summary": get_summary(selector),
+        "sources": get_sources(selector),
+        "categories": get_categories(selector),
     }
 
 
