75030637c014b04521d21c7f90d4fe636bfb7ba4
Edrei <edreimateus@gmail.com>
2022-01-21 01:09:59 -0300

    feat 3 requiriment

--- tech_news/scraper.py
+++ tech_news/scraper.py
@@ -30,6 +30,10 @@ def scrape_novidades(html_content):
 # Requisito 3
 def scrape_next_page_link(html_content):
     """Seu código deve vir aqui"""
+    selector = parsel.Selector(text=html_content)
+    next_page = selector.css(".tec--btn::attr(href)").get()
+
+    return next_page if next_page else None
 
 
 # Requisito 4
