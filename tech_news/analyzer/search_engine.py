87480975314096ae5b780b8072f56fbcb1f6fa0f
rcalvs <rafaelcalvette@gmail.com>
2022-01-21 14:34:42 -0300

    Req 07 OK

--- tech_news/analyzer/search_engine.py
+++ tech_news/analyzer/search_engine.py
@@ -18,12 +18,31 @@ def search_by_title(title):
 # Requisito 7
 def search_by_date(date):
     """Seu código deve vir aqui"""
+    # https://stackoverflow.com/questions/16527878/check-if-line-is-a-timestamp-in-python
+    try:
+        time.strptime(date, "%Y-%m-%d")
+    except ValueError:
+        raise ValueError("Data inválida")
+
+    data = search_news({"timestamp": {"$regex": date}})
+     news_list = []
+    for item in data:
+        news = (item["title"], item["url"])
+         news_list.append(news)
+    return
 
 
 # Requisito 8
 def search_by_source(source):
     """Seu código deve vir aqui"""
+        db_data = search_news({"sources": {"$regex": source, "$options": "i"}})
+    news_list = []
 
+    for item in db_data:
+        news = (item["title"], item["url"])
+        news_list.append(news)
+
+    return news_list
 
 # Requisito 9
 def search_by_category(category):
