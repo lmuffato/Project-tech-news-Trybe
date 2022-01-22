605ba4baf19c0c10c603bf16aaa2ae65c4ab2d4a
moraesvictor <moraesvictor@gmail.com>
2022-01-22 13:30:37 -0300

    feat: def scape_next_page_link

--- tech_news/scraper.py
+++ tech_news/scraper.py
@@ -27,7 +27,10 @@ def scrape_novidades(html_content):
 
 # Requisito 3
 def scrape_next_page_link(html_content):
-    """Seu código deve vir aqui"""
+    selector = Selector(text=html_content)
+
+    next_news_page_link = selector.css('.tec--btn::attr(href)').get()
+    return next_news_page_link
 
 
 # Requisito 4
