630df1f1221cded12d910e7a42a1650439980d02
moraesvictor <moraesvictor@gmail.com>
2022-01-22 08:26:19 -0300

    fix: blank lines

--- tech_news/scraper.py
+++ tech_news/scraper.py
@@ -22,11 +22,9 @@ def scrape_novidades(html_content):
 def scrape_next_page_link(html_content):
     """Seu código deve vir aqui"""
 
-
 # Requisito 4
 def scrape_noticia(html_content):
     """Seu código deve vir aqui"""
-
 
 # Requisito 5
 def get_tech_news(amount):
