f75906d65e1b7ea4de30a8178001fede4adf1dc0
Murilo Gonçalves <murilogoncalvesdev@gmail.com>
2022-01-21 21:33:50 -0300

    feat(req 6,7,8,9): finish

--- tech_news/analyzer/search_engine.py
+++ tech_news/analyzer/search_engine.py
@@ -1,18 +1,57 @@
+from tech_news.database import search_news
+from datetime import datetime
+import re
+
+
+def map_answer(news_list):
+    return [(x["title"], x["url"]) for x in news_list]
+
+
 # Requisito 6
 def search_by_title(title):
-    """Seu código deve vir aqui"""
+    return map_answer(
+        search_news({"title": {"$regex": title, "$options": "i"}})
+    )
 
 
 # Requisito 7
+def parse_date(date):
+    try:
+        regex = re.search(r"(\d{4})-(\d{2})-(\d{2})", date)
+        year, month, day = regex.groups()
+        return datetime(int(year), int(month), int(day)).isoformat()
+    except AttributeError:
+        raise ValueError("Data inválida")
+    except ValueError:
+        raise ValueError("Data inválida")
+
+
 def search_by_date(date):
-    """Seu código deve vir aqui"""
+    parse_date(date)
+    return map_answer(search_news({"timestamp": {"$regex": date}}))
 
 
 # Requisito 8
 def search_by_source(source):
-    """Seu código deve vir aqui"""
+    return map_answer(
+        search_news(
+            {
+                "sources": {
+                    "$elemMatch": {"$regex": source, "$options": "i"}
+                }
+            }
+        )
+    )
 
 
 # Requisito 9
 def search_by_category(category):
-    """Seu código deve vir aqui"""
+    return map_answer(
+        search_news(
+            {
+                "categories": {
+                    "$elemMatch": {"$regex": category, "$options": "i"}
+                }
+            }
+        )
+    )
