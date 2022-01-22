176c771ee15e3df424da43876acee11c4d531cf7
Bidfca <bruninhakjurugat11@gmail.com>
2022-01-22 04:01:25 -0300

    11

--- tech_news/analyzer/ratings.py
+++ tech_news/analyzer/ratings.py
@@ -1,4 +1,5 @@
 from tech_news.database import find_news
+from collections import Counter
 
 
 # Requisito 10
@@ -8,11 +9,21 @@ def top_5_news():
         return []
     news.sort(
         reverse=True,
-        key=lambda new: new["shares_count"] + new["comments_count"]
+        key=lambda new: new['shares_count'] + new['comments_count']
     )
-    return [(new["title"], new["url"]) for new in news[:5]]
+    return [(new['title'], new['url']) for new in news[:5]]
 
 
 # Requisito 11
 def top_5_categories():
-    """Seu código deve vir aqui"""
+    news = find_news()
+    categories = []
+    if not news:
+        return []
+    for new in news:
+        categories.extend([*new['categories']])
+    categories_count = Counter(categories)
+    sorted_by_popularity = sorted(
+        categories_count, key=categories_count.get, reverse=True
+    )
+    return sorted(sorted_by_popularity)[:5]
