0dd8cef44567c5e44dc2b0fdc9d7e328d71739f4
Murilo Gonçalves <murilogoncalvesdev@gmail.com>
2022-01-21 21:58:31 -0300

    feat(req10): implement top_5_news

--- tech_news/analyzer/ratings.py
+++ tech_news/analyzer/ratings.py
@@ -1,6 +1,30 @@
+from tech_news.database import db
+
+
+def map_answer(news_list):
+    return [(x["title"], x["url"]) for x in news_list]
+
+
 # Requisito 10
 def top_5_news():
-    """Seu código deve vir aqui"""
+    set1 = {
+      "$set": {
+        "popularity": {
+          "$add": ["$shares_count", "$comments_count"]
+        }
+      }
+    }
+    sort1 = {
+      "$sort": {
+        "popularity": -1,
+        "title": 1
+      }
+    }
+    limit1 = {
+      "$limit": 5
+    }
+
+    return map_answer(list(db.news.aggregate([set1, sort1, limit1])))
 
 
 # Requisito 11
