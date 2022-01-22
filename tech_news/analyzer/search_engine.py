c1c4c718c74ec8c0453a0294f23c4c4fc92cd338
joaoAndradeJr <fullservicemanaus@gmail.com>
2022-01-21 21:32:13 -0400

    feat: Req 6 - Crie a função search_by_title

--- tech_news/analyzer/search_engine.py
+++ tech_news/analyzer/search_engine.py
@@ -1,6 +1,26 @@
+from tech_news.database import search_news
+
+
 # Requisito 6
 def search_by_title(title):
-    """Seu código deve vir aqui"""
+    # pesquisa sobre busca usando case sensitive
+    # https://docs.mongodb.com/manual/reference/method/db.collection.find/
+    # https://docs.mongodb.com/manual/reference/operator/query/regex/#mongodb-query-op.-regex
+
+    results = search_news({"title": {"$regex": title, "$options": "i"}})
+    response = []
+
+    for result in results:
+        title_search = result['title']
+        url_search = result['url']
+
+        response.append(title_search)
+        response.append(url_search)
+
+    if response:
+        return [tuple(response)]
+    else:
+        return []
 
 
 # Requisito 7
