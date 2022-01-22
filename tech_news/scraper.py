7fcab27cf1d1f18ba77a18ada9eae9f8b49f9a68
moraesvictor <moraesvictor@gmail.com>
2022-01-22 08:30:08 -0300

    fix: for god s sake, skip this blooding line

--- tech_news/scraper.py
+++ tech_news/scraper.py
@@ -18,13 +18,16 @@ def fetch(url):
 def scrape_novidades(html_content):
     """Seu código deve vir aqui"""
 
+
 # Requisito 3
 def scrape_next_page_link(html_content):
     """Seu código deve vir aqui"""
 
+
 # Requisito 4
 def scrape_noticia(html_content):
     """Seu código deve vir aqui"""
+
 
 # Requisito 5
 def get_tech_news(amount):
