859fa51e81e5a9b81385bf427433b313a661de67
Gabriel L Ferreira <78182689+gabriellferreira-dev@users.noreply.github.com>
2022-01-23 12:05:55 -0300

    feat: requiriment 3

--- tech_news/scraper.py
+++ tech_news/scraper.py
@@ -25,7 +25,12 @@ def scrape_novidades(html_content):
 
 # Requisito 3
 def scrape_next_page_link(html_content):
-    """Seu código deve vir aqui"""
+    selector = Selector(text=html_content)
+    link = selector.css(
+        "a.tec--btn::attr(href)"
+    ).getall()
+
+    return link[0] if link else None
 
 
 # Requisito 4
