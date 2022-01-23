40bc65e39ca23f19acfa252ea181646dc6456b5d
Gabriel L Ferreira <78182689+gabriellferreira-dev@users.noreply.github.com>
2022-01-23 12:04:06 -0300

    feat: requiriment 2

--- tech_news/scraper.py
+++ tech_news/scraper.py
@@ -1,6 +1,6 @@
 import time
 import requests
-
+from parsel import Selector
 
 # Requisito 1
 def fetch(url):
@@ -17,7 +17,10 @@ def fetch(url):
 
 # Requisito 2
 def scrape_novidades(html_content):
-    """Seu código deve vir aqui"""
+    selector = Selector(text=html_content)
+    return selector.css(
+        "h3.tec--card__title a.tec--card__title__link::attr(href)"
+    ).getall()
 
 
 # Requisito 3
