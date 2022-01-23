39bc1e756c876400c50e8dcc530420a673bd1587
vinicios <soicinivt@gmail.com>
2022-01-22 21:10:48 -0300

    feat(req 11): function top_5_categories in ratings.py

--- tech_news/analyzer/ratings.py
+++ tech_news/analyzer/ratings.py
@@ -26,4 +26,13 @@ def top_5_news():
 
 # Requisito 11
 def top_5_categories():
-    """Seu código deve vir aqui"""
+    categories = db.news.aggregate(
+        [
+            {"$unwind": "$categories"},
+            {"$group": {"_id": "$categories", "count": {"$sum": 1}}},
+            {"$sort": {"count": -1, "_id": 1}},
+            {"$limit": 5},
+        ]
+    )
+    top_5 = sorted(list(category["_id"] for category in categories))
+    return top_5
