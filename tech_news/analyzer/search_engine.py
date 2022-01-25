f42e639a9c0083987e74e48ded4298f2f28f0821
Felipe Shcolnik <shcolnik@gmail.com.br>
2022-01-24 21:37:14 -0300

    task 6

--- tech_news/analyzer/search_engine.py
+++ tech_news/analyzer/search_engine.py
@@ -1,6 +1,15 @@
+from tech_news.database import find_news
+
+
 # Requisito 6
 def search_by_title(title):
-    """Seu código deve vir aqui"""
+    db_list = find_news()
+    filtered_news = []
+    for news in db_list:
+        if news["title"].upper() == title.upper():
+            print(f'\n noticia {news}')
+            filtered_news.append(news)
+    return [(news["title"], news["url"]) for news in filtered_news]
 
 
 # Requisito 7
