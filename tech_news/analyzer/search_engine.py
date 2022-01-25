5a13443aaf790c3745e5e5b0c8fd399d4257ac32
Edrei <edreimateus@gmail.com>
2022-01-25 15:53:30 -0300

    feat 6 requiriment

--- tech_news/analyzer/search_engine.py
+++ tech_news/analyzer/search_engine.py
@@ -1,6 +1,11 @@
 # Requisito 6
+from tech_news.database import search_news
+
+
 def search_by_title(title):
     """Seu código deve vir aqui"""
+    title_news = search_news({"title": {"$regex": title, "$options": "i"}})
+    return [(new["title"], new["url"]) for new in title_news]
 
 
 # Requisito 7
