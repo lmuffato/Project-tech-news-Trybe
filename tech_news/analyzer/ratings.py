4e8e557b2d0d2effbfb267629c42672626367b05
Breno <blf@ic.ufal.br>
2022-01-25 01:39:23 -0300

    feat: function top_5_news

--- tech_news/analyzer/ratings.py
+++ tech_news/analyzer/ratings.py
@@ -1,6 +1,27 @@
+from tech_news.database import db
+
+
 # Requisito 10
 def top_5_news():
     """Seu código deve vir aqui"""
+    query = [
+        {
+            "$project": {
+                "_id": 0,
+                "title": 1,
+                "url": 1,
+                "total": {"$add": ["$shares_count", "$comments_count"]},
+            },
+        },
+        {
+            "$sort": {
+                "total": -1,
+                "title": 1,
+            }
+        }
+    ]
+    results = db.news.aggregate(query)
+    return [(result["title"], result["url"]) for result in results][:5]
 
 
 # Requisito 11
