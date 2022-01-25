1328e952e6706fc40ff08bac116ad340e09bda49
Edrei <edreimateus@gmail.com>
2022-01-25 16:43:39 -0300

    feat 10 requiriment

--- tech_news/analyzer/ratings.py
+++ tech_news/analyzer/ratings.py
@@ -1,6 +1,22 @@
 # Requisito 10
+from tech_news.database import find_news
+
+
 def top_5_news():
     """Seu código deve vir aqui"""
+    data = find_news()
+    result = []
+
+    data.sort(
+        key=lambda item: (item["shares_count"] + item["comments_count"]),
+        reverse=True
+    )
+
+    for item in data[:5]:
+        new = (item["title"], item["url"])
+        result.append(new)
+
+    return result
 
 
 # Requisito 11
