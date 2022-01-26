99c3ec5c9f99545d81bd09100f1b7c2a5e6f6277
Breno <blf@ic.ufal.br>
2022-01-26 08:10:38 -0300

    feat: analyzer_menu working

--- tech_news/menu.py
+++ tech_news/menu.py
@@ -1,27 +1,77 @@
 import sys
+from tech_news.scraper import get_tech_news
+from tech_news.analyzer.search_engine import (
+    search_by_title,
+    search_by_date,
+    search_by_source,
+    search_by_category,
+)
+from tech_news.analyzer.ratings import top_5_news, top_5_categories
 
 
 def case_zero():
-    input("Digite quantas notícias serão buscadas:\n")
+    user_choice = int(input("Digite quantas notícias serão buscadas:\n"))
+    try:
+        result = get_tech_news(user_choice)
+        return result
+    except Exception as err:
+        return print(f"Deu ruim, toma: {err}", file=sys.stderr)
 
 
 def case_one():
-    input("Digite o título:\n")
+    user_choice = input("Digite o título:\n")
+    try:
+        result = search_by_title(user_choice)
+        return result
+    except Exception as err:
+        return print(f"Deu ruim, toma: {err}", file=sys.stderr)
 
 
 def case_two():
-    input("Digite a data no formato aaaa-mm-dd:\n")
+    user_choice = input("Digite a data no formato aaaa-mm-dd:\n")
+    try:
+        result = search_by_date(user_choice)
+        return result
+    except Exception as err:
+        return print(f"Deu ruim, toma: {err}", file=sys.stderr)
 
 
 def case_three():
-    input("Digite a fonte:\n")
+    user_choice = input("Digite a fonte:\n")
+    try:
+        result = search_by_source(user_choice)
+        return result
+    except Exception as err:
+        return print(f"Deu ruim, toma: {err}", file=sys.stderr)
 
 
 def case_four():
-    input("Digite a categoria:\n")
+    user_choice = input("Digite a categoria:\n")
+    try:
+        result = search_by_category(user_choice)
+        return result
+    except Exception as err:
+        return print(f"Deu ruim, toma: {err}", file=sys.stderr)
+
+
+def case_five():
+    try:
+        result = top_5_news()
+        return result
+    except Exception as err:
+        return print(f"Deu ruim, toma: {err}", file=sys.stderr)
+
+
+def case_six():
+    try:
+        result = top_5_categories()
+        return result
+    except Exception as err:
+        return print(f"Deu ruim, toma: {err}", file=sys.stderr)
 
 
 def case_seven():
+    print("Encerrando script\n")
     sys.stdin.close()
 
 
@@ -39,12 +89,14 @@ def analyzer_menu():
         " 7 - Sair."
     )
     switch_options = {
-         "0": case_zero,
-         "1": case_one,
-         "2": case_two,
-         "3": case_three,
-         "4": case_four,
-         "7": case_seven,
+        "0": case_zero,
+        "1": case_one,
+        "2": case_two,
+        "3": case_three,
+        "4": case_four,
+        "5": case_five,
+        "6": case_six,
+        "7": case_seven,
     }
 
     if chosen_option not in switch_options:
