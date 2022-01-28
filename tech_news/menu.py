86bd99c2940ea4ed641a6ddda8e33c93e5aae35b
Felipe Shcolnik <shcolnik@gmail.com.br>
2022-01-28 16:58:42 -0300

    less complexity of fct

--- tech_news/menu.py
+++ tech_news/menu.py
@@ -2,36 +2,37 @@ import sys
 
 
 # Requisito 12
-def answer_menu(answer):
-    if answer == '0':
-        print("Digite quantas notícias serão buscadas:")
-    if answer == '1':
-        print("Digite o título:")
-    elif answer == '2':
-        print("Digite a data no formato aaaa-mm-dd:")
-    elif answer == '3':
-        print("Digite a fonte:")
-    elif answer == '4':
-        print("Digite a categoria:")
+def option(id, text, answer_input):
+    if answer_input == id:
+        print(text)
+        return True
+    return False
+
+
+def answer_menu(answer_input):
+    if (
+        option("0", "Digite quantas notícias serão buscadas:", answer_input)
+        or option("1", "Digite quantas notícias serão buscadas:", answer_input)
+        or option("2", "Digite quantas notícias serão buscadas:", answer_input)
+        or option("3", "Digite quantas notícias serão buscadas:", answer_input)
+        or option("4", "Digite quantas notícias serão buscadas:", answer_input)
+    ):
+        pass
     else:
         sys.stderr.write("Opção inválida\n")
 
 
 def analyzer_menu():
     menu = (
-            'Selecione uma das opções a seguir:\n' +
-            ' 0 - Popular o banco com notícias;\n' +
-            ' 1 - Buscar notícias por título;\n' +
-            ' 2 - Buscar notícias por data;\n' +
-            ' 3 - Buscar notícias por fonte;\n' +
-            ' 4 - Buscar notícias por categoria;\n' +
-            ' 5 - Listar top 5 notícias;\n' +
-            ' 6 - Listar top 5 categorias;\n' +
-            ' 7 - Sair.'
+        "Selecione uma das opções a seguir:\n"
+        + " 0 - Popular o banco com notícias;\n"
+        + " 1 - Buscar notícias por título;\n"
+        + " 2 - Buscar notícias por data;\n"
+        + " 3 - Buscar notícias por fonte;\n"
+        + " 4 - Buscar notícias por categoria;\n"
+        + " 5 - Listar top 5 notícias;\n"
+        + " 6 - Listar top 5 categorias;\n"
+        + " 7 - Sair."
     )
     option = input(menu)
     answer_menu(option)
-
-
-
-# analyzer_menu()
