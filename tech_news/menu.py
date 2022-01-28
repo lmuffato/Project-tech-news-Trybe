8704d3882313d5f082403a3727fb4584cecec594
Felipe Shcolnik <shcolnik@gmail.com.br>
2022-01-28 14:59:46 -0300

    set menu options

--- tech_news/menu.py
+++ tech_news/menu.py
@@ -1,3 +1,25 @@
 # Requisito 12
 def analyzer_menu():
-    """Seu código deve vir aqui"""
+    enun = 'Selecione uma das opções a seguir:\n'
+    op0 = '0 - Popular o banco com notícias;\n'
+    op1 = '1 - Buscar notícias por título;\n'
+    op2 = '2 - Buscar notícias por data;\n'
+    op3 = '3 - Buscar notícias por fonte;\n'
+    op4 = '4 - Buscar notícias por categoria;\n'
+    op5 = '5 - Listar top 5 notícias;\n'
+    op6 = '6 - Listar top 5 categorias;\n'
+    op7 = '7 - Sair.\n'
+    print(enun + op0 + op1 + op2 + op3 + op4 + op5 + op6 + op7)
+    option = input()
+    if option == '0':
+        print("Digite quantas notícias serão buscadas:")
+    if option == '1':
+        print("Digite o título:")
+    if option == '2':
+        print("Digite a data no formato aaaa-mm-dd:")
+    if option == '3':
+        print("Digite a fonte:")
+    if option == '4':
+        print("Digite a categoria:")
+    else:
+        raise ValueError("Opção inválida")
