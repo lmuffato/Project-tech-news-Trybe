4a19ddad012be952704da014d169a6e0ee5ac2c7
AdelinoJnr <adelinojunior96@gmail.com>
2022-01-29 08:56:05 -0300

    FEAT: Function scrape_next_page_link - Req 3

--- tech_news/scraper.py
+++ tech_news/scraper.py
@@ -26,7 +26,10 @@ def scrape_novidades(html_content):
 
 # Requisito 3
 def scrape_next_page_link(html_content):
-    """Seu código deve vir aqui"""
+    selector = parsel.Selector(html_content)
+    url = selector.css("a.tec--btn--lg ::attr(href)").get()
+
+    return url
 
 
 # Requisito 4
