fca0e6b6661716a8aa25fbd61290a158647bad60
w-cypriano <profwellington17@gmail.com>
2022-01-29 09:06:16 -0300

    req11

--- tech_news/analyzer/ratings.py
+++ tech_news/analyzer/ratings.py
@@ -1,4 +1,5 @@
 from tech_news.database import find_news
+from collections import Counter
 
 
 # Requisito 10
@@ -27,4 +28,18 @@ def top_5_news():
 
 # Requisito 11
 def top_5_categories():
-    """Seu código deve vir aqui"""
+    news_list = []
+    response = find_news()
+    if len(response) == 0:
+        return response
+    for item in response:
+        for cat in item["categories"]:
+            news_list.append(cat)
+    duplicates = Counter(news_list)
+    top_typle = duplicates.most_common()
+    categories_list = []
+    for category in top_typle:
+        categories_list.append(category[0])
+    categories_list.sort()
+    top_5_list = categories_list[:5]
+    return top_5_list
