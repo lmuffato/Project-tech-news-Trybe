e854d67d1adb8d5a8acb81af1ccf2324e12aa9e3
AdelinoJnr <adelinojunior96@gmail.com>
2022-01-29 09:30:25 -0300

    FEAT: Function scrape_noticia - Req 4

--- tech_news/scraper.py
+++ tech_news/scraper.py
@@ -1,6 +1,6 @@
 import requests
 import time
-import parsel
+from parsel import Selector
 
 
 # Requisito 1
@@ -17,7 +17,7 @@ def fetch(url):
 
 # Requisito 2
 def scrape_novidades(html_content):
-    data = parsel.Selector(text=html_content)
+    data = Selector(text=html_content)
     links = data.css(
         "h3.tec--card__title a.tec--card__title__link::attr(href)"
     ).getall()
@@ -26,15 +26,88 @@ def scrape_novidades(html_content):
 
 # Requisito 3
 def scrape_next_page_link(html_content):
-    selector = parsel.Selector(html_content)
+    selector = Selector(html_content)
     url = selector.css("a.tec--btn--lg ::attr(href)").get()
 
     return url
 
 
+def extract_comments(comments):
+    if comments is None:
+        comments = 0
+        return comments
+    else:
+        comments = int(comments)
+        return comments
+
+
+def extract_paragraph(data):
+    graph = data.css("div.tec--article__body > p:first-child *::text").getall()
+    phrase_complete = "".join(graph)
+    return phrase_complete
+
+
+def extract_categories(data):
+    categories_array = []
+    categories = data.css("a.tec--badge--primary ::text").getall()
+
+    for category in categories:
+        categories_array.append(category.strip())
+
+    return categories_array
+
+
+def extract_sources(data):
+    sources_array = []
+    sources = data.css("div.z--mb-16 a::text").getall()
+
+    for source in sources:
+        sources_array.append(source.strip())
+
+    return sources_array
+
+
+def extract_writer(data):
+    if data.css(".tec--author__info__link ::text").get():
+        writer = data.css(".tec--author__info__link ::text").get()
+        return writer
+    if data.css(".tec--timestamp__item a::text").get():
+        writer = data.css(".tec--timestamp__item a::text").get()
+        return writer
+    if data.css(".z--m-none ::text").get():
+        writer = data.css(".z--m-none ::text").get()
+        return writer
+
+
 # Requisito 4
 def scrape_noticia(html_content):
-    """Seu código deve vir aqui"""
+    data = Selector(html_content)
+    url = data.css("head link[rel=canonical] ::attr(href)").get()
+    title = data.css("h1.tec--article__header__title ::text").get()
+    timestamp = data.css("div.tec--timestamp__item ::attr(datetime)").get()
+    writer_tech = extract_writer(data)
+    writer = writer_tech.strip() if writer_tech else None
+    shares_se = "#js-author-bar > nav > div:nth-child(1)::text"
+    shares_tech = data.css(shares_se).re_first(r'\d+')
+    shares = int(shares_tech) if shares_tech else 0
+    comments = data.css("#js-comments-btn ::text").re_first(r'\d+')
+    comments_count = int(comments) if comments else 0
+    summary = extract_paragraph(data)
+    sources = extract_sources(data)
+    categories = extract_categories(data)
+
+    news_json = {
+      "url": url,
+      "title": title,
+      "timestamp": timestamp,
+      "writer": writer,
+      "shares_count": int(shares),
+      "comments_count": comments_count,
+      "summary": summary,
+      "sources": sources,
+      "categories": categories
+    }
+    return news_json
 
 
 # Requisito 5
