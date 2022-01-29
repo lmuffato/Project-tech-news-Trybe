0b1666ba3f05c875b23c8c6054f0fee1432eaffd
AdelinoJnr <adelinojunior96@gmail.com>
2022-01-29 09:48:39 -0300

    FEAT: Function search_by_date - Req 7

--- tech_news/analyzer/search_engine.py
+++ tech_news/analyzer/search_engine.py
@@ -1,4 +1,5 @@
 from tech_news.database import search_news
+from datetime import datetime
 
 
 # Requisito 6
@@ -15,7 +16,19 @@ def search_by_title(title):
 
 # Requisito 7
 def search_by_date(date):
-    """Seu código deve vir aqui"""
+    try:
+        datetime.strptime(date, "%Y-%m-%d")
+    except ValueError:
+        raise ValueError("Data inválida")
+
+    find_news = search_news({"timestamp": {"$regex": date, "$options": "i"}})
+    list_news = []
+
+    for news in find_news:
+        result = (news["title"], news["url"])
+        list_news.append(result)
+
+    return list_news
 
 
 # Requisito 8
