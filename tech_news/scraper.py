b99050ed007457a60393bdeac8d103902a8888d7
Southier <patrick.southier@hotmail.com>
2022-01-29 23:30:26 -0300

    feat: creating the scrape_novidades function

--- tech_news/scraper.py
+++ tech_news/scraper.py
@@ -1,5 +1,6 @@
 import requests
 import time
+from parsel import Selector
 
 
 # Requisito 1
@@ -16,7 +17,9 @@ def fetch(url):
 
 # Requisito 2
 def scrape_novidades(html_content):
-    """Seu código deve vir aqui"""
+    selector = Selector(text=html_content)
+    news_list = selector.css(".tec--card__info h3 a::attr(href)").getall()
+    return news_list
 
 
 # Requisito 3
