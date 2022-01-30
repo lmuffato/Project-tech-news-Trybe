d949d35062e730676156b69a087a5372877ffb3c
fomes <fgomesdeluna@gmail.com>
2022-01-29 21:01:12 -0300

    start project tech news

--- tech_news/scraper.py
+++ tech_news/scraper.py
@@ -1,6 +1,18 @@
+import requests  # permite realizar requisições http
+import time  # acesso a horário e conversões
+
+
 # Requisito 1
-def fetch(url):
-    """Seu código deve vir aqui"""
+def fetch(url):  # função fetch recebe uma url
+    try:
+        # requisição http definindo timeout de 3seg
+        response = requests.get(url, timeout=3)
+        time.sleep(1)  # rate limite 1 segundo
+        if response.status_code != 200:  # retorna um number
+            return None  # retorna none se status_code for diferente de 200
+        return response.text  # retorna conteúdo de texto caso status seja 200
+    except requests.Timeout:  # caso não receba resposta no time especificado..
+        return None  # retorna none
 
 
 # Requisito 2
