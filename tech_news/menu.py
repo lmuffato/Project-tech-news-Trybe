aedeb79afac893f482589463d4f787051e6e3583
luizfelipe-dev <thelastnailmusic@gmail.com>
2022-01-30 15:39:39 -0300

    menu em progresso

--- tech_news/menu.py
+++ tech_news/menu.py
@@ -1,3 +1,17 @@
 # Requisito 12
+
+
 def analyzer_menu():
-    """Seu código deve vir aqui"""
+    options = input(
+        "Selecione uma das opções a seguir:\n"
+        " 0 - Popular o banco com notícias;\n"
+        " 1 - Buscar notícias por título;\n"
+        " 2 - Buscar notícias por data;\n"
+        " 3 - Buscar notícias por fonte;\n"
+        " 4 - Buscar notícias por categoria;\n"
+        " 5 - Listar top 5 notícias;\n"
+        " 6 - Listar top 5 categorias;\n"
+        " 7 - Sair.\n"
+    )
+
+    return options
