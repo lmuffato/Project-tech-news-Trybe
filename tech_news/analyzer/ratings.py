eea48f12743fbe7b609e1ce1b936b5b23a7d3486
ScriptCamilo <script.camilo@gmail.com>
2022-01-30 20:00:18 -0300

    Tenth requirement done

--- tech_news/analyzer/ratings.py
+++ tech_news/analyzer/ratings.py
@@ -1,6 +1,22 @@
+from tech_news.database import find_news
+
+
 # Requisito 10
 def top_5_news():
-    """Seu código deve vir aqui"""
+    news = find_news()
+    cmnts = 'comments_count'
+    shares = 'shares_count'
+    tops = sorted([
+        (new[cmnts] + new[shares], new['title'], new['url']) for new in news
+    ], reverse=True)
+    tops_5 = []
+    for n in range(len(tops)):
+        if n > 4:
+            break
+        new = tops[n]
+        tops_5.append((new[1], new[2]))
+    return tops_5
+
 
 
 # Requisito 11
