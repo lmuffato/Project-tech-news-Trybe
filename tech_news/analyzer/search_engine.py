de32abf145d3f832923659de89aab483641c76e1
ScriptCamilo <script.camilo@gmail.com>
2022-01-30 11:14:11 -0300

    Sixth requirement done

--- tech_news/analyzer/search_engine.py
+++ tech_news/analyzer/search_engine.py
@@ -1,6 +1,16 @@
+from tech_news.database import search_news
+
+
 # Requisito 6
 def search_by_title(title):
-    """Seu código deve vir aqui"""
+    query_capitalized = {'title': title.capitalize()}
+    query_lowered = {'title': title.lower()}
+    news = (
+        search_news(query_lowered)
+        or search_news(query_capitalized)
+    )
+    news_tuple = [(new['title'], new['url']) for new in news]
+    return news_tuple
 
 
 # Requisito 7
