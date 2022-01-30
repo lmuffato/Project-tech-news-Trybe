a506f6bcb3e87827be0ed3df699d3f9bcd14249b
lucas-paz <lucas.hpaz@outlook.com>
2022-01-30 20:51:39 -0300

    req 2

--- tech_news/scraper.py
+++ tech_news/scraper.py
@@ -6,7 +6,7 @@ from .database import create_news
 
 # Requisito 1
 def fetch(url):
-    time.sleep(2)
+    time.sleep(1)
     try:
         response = requests.get(url, timeout=3)
         if (response.status_code == 200):
@@ -18,12 +18,20 @@ def fetch(url):
 
 # Requisito 2
 def scrape_novidades(html_content):
-    """Seu código deve vir aqui"""
+    content = Selector(html_content)
+    return content.css("h3.tec--card__title a::attr(href)").getall()
 
 
 # Requisito 3
 def scrape_next_page_link(html_content):
-    """Seu código deve vir aqui"""
+    page = Selector(html_content)
+    css_next_page_selector = ".tec--list a.tec--btn::attr(href)"
+    next_page_button = page.css(css_next_page_selector).get()
+
+    if (next_page_button):
+        return next_page_button
+    else:
+        return None
 
 
 # Requisito 4
