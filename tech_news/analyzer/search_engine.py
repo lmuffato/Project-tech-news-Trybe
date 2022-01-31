d280de9095f944247b17eb2fe69a0a14cfcf4401
Parker9309 <juan.tentiomaru9309@gmail.com>
2022-01-31 00:59:57 -0300

    feat: req 7 done test

--- tech_news/analyzer/search_engine.py
+++ tech_news/analyzer/search_engine.py
@@ -1,4 +1,5 @@
 from tech_news.database import search_news
+from datetime import datetime
 
 
 # Requisito 6
@@ -9,7 +10,15 @@ def search_by_title(title):
 
 # Requisito 7
 def search_by_date(date):
-    """Seu código deve vir aqui"""
+    try:
+        datetime.strptime(date, "%Y-%m-%d")
+    except ValueError:
+        raise ValueError("Data inválida")
+    filters_list = search_news({"timestamp": {"$regex": date}})
+    list = []
+    for news in filters_list:
+        list.append((news["title"], news["url"]))
+    return list
 
 
 # Requisito 8
