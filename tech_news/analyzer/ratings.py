3631ca6328b9841188a6a63a1784855803067983
lucas-paz <lucas.hpaz@outlook.com>
2022-01-30 21:33:39 -0300

    req 10

--- tech_news/analyzer/ratings.py
+++ tech_news/analyzer/ratings.py
@@ -1,6 +1,16 @@
+from ..database import find_news
+
+
 # Requisito 10
+# Ref https://github.com/tryber/sd-010-a-tech-news/pull/65/files
 def top_5_news():
-    """Seu código deve vir aqui"""
+    results = find_news()
+    results.sort(
+        key=lambda new: (new["shares_count"] + new["comments_count"]),
+        reverse=True
+        )
+    top_5 = results[:5]
+    return [(news['title'], news['url']) for news in top_5]
 
 
 # Requisito 11
