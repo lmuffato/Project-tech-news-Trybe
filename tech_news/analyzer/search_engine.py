53a86300ed28e114b1bb18e8a836d2796a770853
lucas-paz <lucas.hpaz@outlook.com>
2022-01-30 21:23:06 -0300

    req 8 e 9

--- tech_news/analyzer/search_engine.py
+++ tech_news/analyzer/search_engine.py
@@ -21,9 +21,11 @@ def search_by_date(date):
 
 # Requisito 8
 def search_by_source(source):
-    """Seu código deve vir aqui"""
+    result = search_news({"sources": {"$regex": source, "$options": "i"}})
+    return [(news['title'], news['url']) for news in result]
 
 
 # Requisito 9
 def search_by_category(category):
-    """Seu código deve vir aqui"""
+    result = search_news({"categories": {"$regex": category, "$options": "i"}})
+    return [(news['title'], news['url']) for news in result]
