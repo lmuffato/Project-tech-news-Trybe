e9fe91015a94e58415e9d104d75f51cc7de2abec
carol-bitencourt <carolmedeirosbitencourt@gmail.com>
2022-01-30 21:46:41 -0300

    Req 3

--- tech_news/scraper.py
+++ tech_news/scraper.py
@@ -26,7 +26,9 @@ def scrape_novidades(html_content):
 
 # Requisito 3
 def scrape_next_page_link(html_content):
-    """Seu código deve vir aqui"""
+    selector = Selector(text=html_content)
+    next_page_url = selector.css(".tec--btn::attr(href)").get()
+    return next_page_url if next_page_url else None
 
 
 # Requisito 4
