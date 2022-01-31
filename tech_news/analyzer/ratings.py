e6ade2f99579410b65c1b7697bc0e0f1a7f73745
lucas-paz <lucas.hpaz@outlook.com>
2022-01-30 21:42:16 -0300

    req 11

--- tech_news/analyzer/ratings.py
+++ tech_news/analyzer/ratings.py
@@ -1,4 +1,5 @@
 from ..database import find_news
+from collections import Counter
 
 
 # Requisito 10
@@ -7,12 +8,23 @@ def top_5_news():
     results = find_news()
     results.sort(
         key=lambda new: (new["shares_count"] + new["comments_count"]),
-        reverse=True
-        )
+        reverse=True,
+    )
     top_5 = results[:5]
-    return [(news['title'], news['url']) for news in top_5]
+    return [(news["title"], news["url"]) for news in top_5]
 
 
 # Requisito 11
 def top_5_categories():
-    """Seu código deve vir aqui"""
+    results = find_news()
+    categories = []
+
+    if not results:
+        return []
+
+    for notice in results:
+        categories.append(notice["categories"])
+
+    count = Counter(categories)
+    sorted_list = sorted(count, key=count.get, reverse=True)
+    return sorted(sorted_list)[:5]
