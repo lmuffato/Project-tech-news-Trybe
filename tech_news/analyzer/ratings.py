ba4ada68cc2cfaa11317485e2c2abd4561aa3f8d
Parker9309 <juan.tentiomaru9309@gmail.com>
2022-01-31 01:32:39 -0300

    feat: req 10 done

--- tech_news/analyzer/ratings.py
+++ tech_news/analyzer/ratings.py
@@ -1,6 +1,16 @@
+from tech_news.database import find_news
+
+
 # Requisito 10
 def top_5_news():
-    """Seu código deve vir aqui"""
+    list = find_news()
+    if not list:
+        return []
+    list.sort(
+        reverse=True,
+        key=lambda new: new["shares_count"] + new["comments_count"],
+    )
+    return [(new["title"], new["url"]) for new in list[:5]]
 
 
 # Requisito 11
