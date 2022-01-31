033a2a76133b722c665fec65e28bfbac5ce8df9c
carol-bitencourt <carolmedeirosbitencourt@gmail.com>
2022-01-31 19:33:30 -0300

    Req 7

--- tech_news/analyzer/search_engine.py
+++ tech_news/analyzer/search_engine.py
@@ -1,5 +1,6 @@
 import re
 from tech_news.database import search_news
+from datetime import datetime
 
 
 # Requisito 6
@@ -12,7 +13,16 @@ def search_by_title(title):
 
 # Requisito 7
 def search_by_date(date):
-    """Seu código deve vir aqui"""
+    try:
+        news = []
+        datetime.strptime(date, "%Y-%m-%d")
+        response_search = search_news({"timestamp": re.compile(date)})
+        if response_search:
+            for noticia in response_search:
+                news.append((noticia["title"], noticia["url"]))
+        return news
+    except ValueError:
+        raise ValueError("Data inválida")
 
 
 # Requisito 8
