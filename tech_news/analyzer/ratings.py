fdcec1900be9370be02fd3c02ca60958f6b8b644
Ederson S <edersonrodrigo31@gmail.com>
2022-01-30 21:57:01 -0300

    feat: requisito 10

--- tech_news/analyzer/ratings.py
+++ tech_news/analyzer/ratings.py
@@ -1,6 +1,25 @@
+from tech_news.database import db
+
+
 # Requisito 10
 def top_5_news():
-    """Seu código deve vir aqui"""
+    news = list(
+        db.news.aggregate(
+            [
+                {
+                    "$sort": {
+                        "comments_count": -1,
+                        "shares_count": -1,
+                        "title": 1,
+                    }
+                },
+                {"$limit": 5},
+                {"$project": {"title": 1, "_id": 0, "url": 1}},
+            ]
+        )
+    )
+    titles_and_urls = [(new["title"], new["url"]) for new in news]
+    return titles_and_urls
 
 
 # Requisito 11
