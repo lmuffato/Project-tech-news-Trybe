f4796a15cf376c5a69e2e0638437e4403581c66a
carol-bitencourt <carolmedeirosbitencourt@gmail.com>
2022-01-31 20:41:27 -0300

    Req 10

--- tech_news/analyzer/ratings.py
+++ tech_news/analyzer/ratings.py
@@ -1,6 +1,18 @@
+from tech_news.database import find_news
+
+
+def total_calculate(new):
+    return new["shares_count"] + new["comments_count"]
+
+
 # Requisito 10
 def top_5_news():
-    """Seu código deve vir aqui"""
+    news_list = find_news()
+    if not news_list:
+        return []
+
+    news_list.sort(reverse=True, key=total_calculate)
+    return [(new["title"], new["url"]) for new in news_list[:5]]
 
 
 # Requisito 11
