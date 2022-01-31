9962eeb46c1f0a06fce410dd77537e06b7852abd
lucas-paz <lucas.hpaz@outlook.com>
2022-01-30 21:17:53 -0300

    req 6

--- tech_news/analyzer/search_engine.py
+++ tech_news/analyzer/search_engine.py
@@ -1,6 +1,10 @@
+from ..database import search_news
+from datetime import datetime
+
 # Requisito 6
 def search_by_title(title):
-    """Seu código deve vir aqui"""
+    results = search_news({"title": {"$regex": title, "$options": "i"}})
+    return [(item['title'], item['url']) for item in results]
 
 
 # Requisito 7
