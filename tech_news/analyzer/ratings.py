d9040edb4434a66faf388cc7a67c513bf7a40efe
Parker9309 <juan.tentiomaru9309@gmail.com>
2022-01-31 01:34:48 -0300

    feat: req 11  done

--- tech_news/analyzer/ratings.py
+++ tech_news/analyzer/ratings.py
@@ -1,4 +1,5 @@
 from tech_news.database import find_news
+from collections import Counter
 
 
 # Requisito 10
@@ -15,4 +16,12 @@ def top_5_news():
 
 # Requisito 11
 def top_5_categories():
-    """Seu código deve vir aqui"""
+    list = find_news()
+    categories = []
+    if not list:
+        return []
+    for gokuSsj in list:
+        categories.extend([*gokuSsj["categories"]])
+    cont = Counter(categories)
+    sort = sorted(cont, key=cont.get, reverse=True)
+    return sorted(sort)[:5]
