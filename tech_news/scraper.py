aa873b1c7c93dfe13063a5a5b13509d28d89da7a
Alana Monteiro <lanaah.15@gmail.com>
2022-02-01 20:40:55 -0300

    Alana Monteiro - Req 1 - 3

--- tech_news/scraper.py
+++ tech_news/scraper.py
@@ -1,16 +1,38 @@
+import time
+import requests
+from parsel import Selector
+
+
 # Requisito 1
 def fetch(url):
-    print(url)
+    try:
+        time.sleep(1)
+        response = requests.get(url, timeout=3)
+        if response.status_code != 200:
+            return None
+        else:
+            return response.text
+    except requests.Timeout:
+        return None
 
 
 # Requisito 2
 def scrape_novidades(html_content):
-    """Seu código deve vir aqui"""
+    selector = Selector(text=html_content)
+    # seletor css do tecmundo: h3.tec--card__title  a.tec--card__title__link
+    news_links = selector.css(
+        'h3.tec--card__title a.tec--card__title__link::attr(href)'
+        ).getall()
+    return news_links
 
 
 # Requisito 3
 def scrape_next_page_link(html_content):
-    """Seu código deve vir aqui"""
+    selector = Selector(text=html_content)
+    # seletor css do tecmundo:
+    # a.tec--btn.tec.btn--lg.tec--btn
+    next_link = selector.css('a.tec--btn--lg::attr(href)').get()
+    return next_link
 
 
 # Requisito 4
