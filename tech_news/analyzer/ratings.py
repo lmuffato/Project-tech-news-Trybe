1e8a09f13128bc549634403a633c12eb25b17e64
phlopes <phlopes93@gmail.com>
2022-02-01 22:42:18 -0300

    req 10 e 11 completed

--- tech_news/analyzer/ratings.py
+++ tech_news/analyzer/ratings.py
@@ -1,8 +1,31 @@
+from tech_news.database import find_news
+from collections import Counter
+
+
 # Requisito 10
 def top_5_news():
-    """Seu código deve vir aqui"""
+    news_list = find_news()
+    if not news_list:
+        return []
+    news_list.sort(
+        reverse=True,
+        key=lambda new: new['shares_count'] + new['comments_count']
+    )
+    return [(new['title'], new['url']) for new in news_list[:5]]
 
 
 # Requisito 11
 def top_5_categories():
-    """Seu código deve vir aqui"""
+    news_list = find_news()
+    categories = []
+    if not news_list:
+        return []
+    for new in news_list:
+        categories.extend([*new['categories']])
+    cont = Counter(categories)
+    sorted_by_popularity = sorted(
+        cont, key=cont.get, reverse=True
+    )
+    return sorted(sorted_by_popularity)[:5]
+
+    # Obtive ajuda do Lucas Godoy nestes requisitos
