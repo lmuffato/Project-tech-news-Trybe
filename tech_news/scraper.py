4c30b690a089fd99d673aba7bd606997455c93f6
Alana Monteiro <lanaah.15@gmail.com>
2022-02-01 21:34:14 -0300

    req 4

--- tech_news/scraper.py
+++ tech_news/scraper.py
@@ -30,14 +30,58 @@ def scrape_novidades(html_content):
 def scrape_next_page_link(html_content):
     selector = Selector(text=html_content)
     # seletor css do tecmundo:
-    # a.tec--btn.tec.btn--lg.tec--btn
+    # tec--btn tec--btn--lg tec--btn--primary z--mx-auto z--mt-48
     next_link = selector.css('a.tec--btn--lg::attr(href)').get()
     return next_link
 
 
 # Requisito 4
 def scrape_noticia(html_content):
-    """Seu código deve vir aqui"""
+    selector = Selector(text=html_content)
+
+    url = selector.css('head link[rel=canonical]::attr(href)').get()
+    title = selector.css('h1.tec--article__header__title::text').get()
+    timestamp = selector.css(
+        'div.tec--timestamp__item time::attr(datetime)'
+        ).get()
+    writer = selector.css('.z--font-bold *::text').get()
+
+    if writer:
+        writer = writer.strip()
+
+    shares_count = selector.css('svg.feather::text').get()
+    if shares_count is None:
+        shares_count = int(0)
+    else:
+        shares_count = int(shares_count)
+    comments_count = selector.css(
+        '#js-comments-btn::attr(data-count)'
+        ).get()
+    if comments_count is None:
+        comments_count = int(0)
+    else:
+        comments_count = int(comments_count)
+    # Summary consultado em outros códigos
+    summary = ''.join(
+        selector.css('.tec--article__body > p:first-child *::text').getall()
+    )
+    sources = selector.css('div.z--mb-16 a::text').getall()
+    sources = [source.strip() for source in sources]
+    categories = selector.css(
+        '#js-categories a.tec--badge *::text'
+        ).getall()
+    categories = [category.strip() for category in categories]
+    return {
+        "url": url,
+        "title": title,
+        "timestamp": timestamp,
+        "writer": writer,
+        "shares_count": shares_count,
+        "comments_count": comments_count,
+        "summary": summary,
+        "sources": sources,
+        "categories": categories,
+        }
 
 
 # Requisito 5
